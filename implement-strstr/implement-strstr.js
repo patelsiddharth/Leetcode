d16a489f93f844e28024ad6951d01af90ecdbe92
Siddharth Patel <siddharthpatel425@gmail.com>
2021-11-29 10:56:27 +0530

    Time: 72 ms (90.35%), Space: 40 MB (76.45%) - LeetHub

---
+++ implement-strstr/implement-strstr.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    if(needle)
+        return haystack.indexOf(needle);
+    return 0;
+};
