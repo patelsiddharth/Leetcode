3fc3cfa2c8d8ab6b549d02000602bc0d59410ed4
Siddharth Patel <siddharthpatel425@gmail.com>
2022-01-09 20:05:09 +0530

    Time: 390 ms (12.90%), Space: 49 MB (30.65%) - LeetHub

--- 1154-day-of-the-year/1154-day-of-the-year.js
+++ 1154-day-of-the-year/1154-day-of-the-year.js
@@ -3,24 +3,20 @@
  * @return {number}
  */
 var dayOfYear = function(date) {
-    const year = parseInt(date.substring(0, 4)),
-          month = parseInt(date[5] == "0" ? date[6] : date.substring(5, 7)),
-          day = parseInt(date[8] == "0" ? date[9] : date.substring(8)),
-          daysInEachMonth = [31,28,31,30,31,30,31,31,30,31,30,31];
+    let year = parseInt(date.substring(0, 4)),
+        month = parseInt(date[5] == "0" ? date[6] : date.substring(5, 7)),
+        day = parseInt(date[8] == "0" ? date[9] : date.substring(8)),
+        daysInEachMonth = [31,28,31,30,31,30,31,31,30,31,30,31];
     
     if((year%4 == 0 && year%100 != 0) || year%400 == 0)
     {
         daysInEachMonth[1] = 29;
     }
     
-    let res = 0;
-    
     for(let i = 0; i < month-1; i++)
     {
-        res += daysInEachMonth[i];
+        day += daysInEachMonth[i];
     }
     
-    res += day
-    
-    return res;
+    return day;
 };
