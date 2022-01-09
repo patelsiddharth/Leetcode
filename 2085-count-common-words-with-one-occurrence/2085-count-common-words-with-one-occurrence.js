fb67f8ad14964a24aae9b5a2cfd1d38448d6d793
Siddharth Patel <siddharthpatel425@gmail.com>
2022-01-09 12:04:10 +0530

    Time: 84 ms (90.27%), Space: 43.9 MB (52.01%) - LeetHub

--- 2085-count-common-words-with-one-occurrence/2085-count-common-words-with-one-occurrence.js
+++ 2085-count-common-words-with-one-occurrence/2085-count-common-words-with-one-occurrence.js
@@ -4,7 +4,7 @@
  * @return {number}
  */
 var countWords = function(words1, words2) {
-    let count = 0, obj1 = {}, obj2 = {};
+    let obj1 = {}, obj2 = {};
     words1.map(word => {
         obj1[word] = obj1[word] ? obj1[word] + 1 : 1;
     })
@@ -12,12 +12,5 @@ var countWords = function(words1, words2) {
         obj2[word] = obj2[word] ? obj2[word] + 1 : 1;
     })
     
-    for (var key of Object.keys(obj2)) 
-    {
-        if(obj1[key] && obj2[key] && (obj1[key] + obj2[key] === 2))
-        {
-            count++;
-        }
-    }
-    return count;
+    return Object.keys(obj1).filter(key => obj1[key] && obj2[key] && (obj1[key] + obj2[key] === 2)).length;
 };
