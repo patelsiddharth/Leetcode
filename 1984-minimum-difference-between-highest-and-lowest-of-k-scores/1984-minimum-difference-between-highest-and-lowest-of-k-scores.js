9125eb8661da80a74b2fcbecdba15f06111f1c21
Siddharth Patel <siddharthpatel425@gmail.com>
2022-01-17 22:42:04 +0530

    Time: 165 ms (8.25%), Space: 40.6 MB (90.72%) - LeetHub

--- 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.js
+++ 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.js
@@ -8,18 +8,13 @@ var minimumDifference = function(nums, k) {
         return 0;
     
     nums.sort((a,b) => b - a);
-    console.log(nums)
     
     let min = nums[0];
     for(let i = 0; i <= nums.length - k; i++)
     {
-        console.log("i : ", i)
-        console.log("nums["+ i +"] : ", nums[i])
-        console.log("nums["+ (i+k-1) +"] : ", nums[i+k-1])
         if(nums[i] - nums[i+k-1] < min)
         {
             min = nums[i] - nums[i+k-1];
-            console.log("MIN : ", min)
         }    
     }
     return min;
