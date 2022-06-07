8f0eb96dfcebcb46486964dcf2c0d81dae6fa69f
Siddharth Patel <siddharthpatel425@gmail.com>
2022-06-07 23:21:46 +0530

    Time: 140 ms (54.78%), Space: 48.2 MB (92.00%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.js
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.js
@@ -3,35 +3,20 @@
  * @return {number[]}
  */
 var sortedSquares = function(nums) {
-    return nums.map(item => item*item).sort((a,b) => a-b)
-//     let start = 0, end = nums.length - 1;
-//     while(end >= start)
-//     {
-//         if(nums[start] < 0 && nums[end] < 0)
-//         {
-//             const temp = nums[end];
-//             nums[end] = nums[start] * nums[start];
-//             nums[start] = temp * temp;
-//             start++;
-//             end--;
-//         }
-//         else
-//         {
-//             if(nums[end] * nums[end] >= nums[start] * nums[start])
-//             {
-//                 nums[end] = nums[end] * nums[end];
-//             }
-//             else
-//             {
-//                 let temp = nums[end];
-//                 nums[end] = nums[start] * nums[start];
-//                 nums[start] = temp;
-//             }
-//             end--;
-//         }
-//         if(nums[start] === 0)
-//             start++;
-            
-//     }
-//     return nums
+    let newArray = new Array(nums.length).fill(0)
+    
+    let start = 0, end = nums.length-1
+    for(let i = end; i >= 0; i--){
+        if(nums[start] * nums[start] > nums[end] * nums[end])
+        {
+            newArray[i] = nums[start] * nums[start]
+            start++
+        }
+        else
+        {
+            newArray[i] = nums[end] * nums[end]
+            end--
+        }
+    }
+    return newArray
 };
