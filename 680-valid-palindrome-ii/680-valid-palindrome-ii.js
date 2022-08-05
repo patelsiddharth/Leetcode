a92f9478241743f8d465466eb4c65a9a7dbd24fa
Siddharth Patel <siddharthpatel425@gmail.com>
2022-08-06 00:46:16 +0530

    Time: 101 ms (81.99%), Space: 48.1 MB (61.45%) - LeetHub

---
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.js
@@ -0,0 +1,39 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var validPalindrome = function(s) {
+    let left = 0, right = s.length - 1, res = true;
+    while(left < right)
+    {
+        if(s[left] === s[right])
+        {
+            left++;
+            right--;
+        }
+        else 
+        {
+            res = isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+            break;
+        }
+    }
+    return res;
+};
+
+function isPalindrome(str, start, end) {
+    let res = true;
+    while(start < end)
+    {
+        if(str[start] === str[end])
+        {
+            start++;
+            end--;
+        }
+        else
+        {
+            res = false;
+            break;
+        }
+    }
+    return res;
+}
