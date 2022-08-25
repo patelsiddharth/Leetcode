c348cbdc4d4a72efee7d86df7aa76baa69f9ee4e
Siddharth Patel <siddharthpatel425@gmail.com>
2022-08-25 20:24:12 +0530

    Time: 77 ms (77.87%), Space: 44.1 MB (32.34%) - LeetHub

---
+++ 1935-maximum-number-of-words-you-can-type/1935-maximum-number-of-words-you-can-type.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} text
+ * @param {string} brokenLetters
+ * @return {number}
+ */
+var canBeTypedWords = function(text, brokenLetters) {
+    let res = 0, obj = {};
+    brokenLetters.split("").forEach(letter => {
+        obj[letter] = 1;
+    })
+    console.log(obj)
+    
+    text.split(" ").forEach(word => {
+        let temp = true;
+        for(let char of word)
+        {
+            if(obj[char])
+            {
+                temp = false;
+                break;
+            }
+        }
+        if(temp)
+        {
+            res++;
+        }
+    })
+    
+    return res;
+};
