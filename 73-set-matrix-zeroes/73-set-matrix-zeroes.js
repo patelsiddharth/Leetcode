a33cbe223488cf2a5cb882909ff0cc0fb959692a
Siddharth Patel <siddharthpatel425@gmail.com>
2022-08-31 12:17:05 +0530

    Time: 277 ms (5.08%), Space: 58.6 MB (5.20%) - LeetHub

---
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var setZeroes = function(matrix) {
+    let row = matrix.length - 1, col = matrix[0].length - 1, visitedMap = new Map();
+    
+    for(let i = 0; i <= row; i++)
+    {
+        for(let j = 0; j <= col; j++)
+        {
+            if(matrix[i][j] === 0 && !visitedMap[[i,j]])
+            {
+                for(let k = 0; k < matrix[i].length; k++)
+                {
+                    if(matrix[i][k] !== 0)
+                    {
+                        matrix[i][k] = 0;
+                        visitedMap[[i,k]] = 1;
+                    }
+                }
+                for(let k = 0; k < matrix.length; k++)
+                {
+                    if(matrix[k][j] !== 0)
+                    {
+                        matrix[k][j] = 0;
+                        visitedMap[[k,j]] = 1;
+                    }
+                }
+            }
+            
+            visitedMap[[i,j]] = 1;
+        }
+    }
+    console.log(matrix);
+};
