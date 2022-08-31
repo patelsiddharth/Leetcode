bbdf372a3ea8b9868ce63f76e928cf0dd0f28024
Siddharth Patel <siddharthpatel425@gmail.com>
2022-08-31 13:02:35 +0530

    Time: 81 ms (96.82%), Space: 44.6 MB (83.15%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.js
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.js
@@ -3,33 +3,31 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var setZeroes = function(matrix) {
-    let row = matrix.length - 1, col = matrix[0].length - 1, visitedMap = new Map();
+    let col0 = 1; row = matrix.length - 1, col = matrix[0].length - 1;
     
     for(let i = 0; i <= row; i++)
     {
-        for(let j = 0; j <= col; j++)
+        if(matrix[i][0] === 0)
+            col0 = 0;
+        
+        for(let j = 1; j <= col; j++)
         {
-            if(matrix[i][j] === 0 && !visitedMap[[i,j]])
+            if(matrix[i][j] === 0)
             {
-                for(let k = 0; k < matrix[i].length; k++)
-                {
-                    if(matrix[i][k] !== 0)
-                    {
-                        matrix[i][k] = 0;
-                        visitedMap[[i,k]] = 1;
-                    }
-                }
-                for(let k = 0; k <= row; k++)
-                {
-                    if(matrix[k][j] !== 0)
-                    {
-                        matrix[k][j] = 0;
-                        visitedMap[[k,j]] = 1;
-                    }
-                }
+                matrix[i][0] = 0;
+                matrix[0][j] = 0
             }
-            
-            visitedMap[[i,j]] = 1;
         }
+    }
+    
+    for(let i = row; i >= 0; i--)
+    {
+        for(let j = col; j >= 1; j--)
+        {
+            if(matrix[i][0] === 0 || matrix[0][j] === 0)
+                matrix[i][j] = 0;
+        }
+        if(col0 === 0)
+            matrix[i][0] = 0
     }
 };
