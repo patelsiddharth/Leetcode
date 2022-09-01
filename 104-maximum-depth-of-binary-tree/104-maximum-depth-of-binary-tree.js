03c3908f8509a43ef9f95963e4ba4d283e31efd7
Siddharth Patel <siddharthpatel425@gmail.com>
2022-09-01 22:46:34 +0530

    Time: 112 ms (36.47%), Space: 45.2 MB (53.58%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.js
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.js
@@ -14,5 +14,23 @@ var maxDepth = function(root) {
     if(root === null)
         return 0;
     
-    return 1 + Math.max(maxDepth(root.right), maxDepth(root.left))
+    let queue = [root], depth = 0;
+    
+    while(queue.length > 0)
+    {
+        let size = queue.length;
+        for(let i = 0; i < size; i++)
+        {
+            let temp = queue.shift();
+            
+            if(temp.left !== null)
+                queue.push(temp.left)
+            
+            if(temp.right !== null)
+                queue.push(temp.right)
+        }
+        depth++;
+    }
+    
+    return depth;
 };
