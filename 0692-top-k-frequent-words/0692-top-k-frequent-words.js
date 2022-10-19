c47c75c98f011c43b1423d8b63a0e61af1e55d8f
Siddharth Patel <siddharthpatel425@gmail.com>
2022-10-19 23:20:22 +0530

    Time: 111 ms (74.78%), Space: 47.2 MB (37.39%) - LeetHub

---
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string[]} words
+ * @param {number} k
+ * @return {string[]}
+ */
+var topKFrequent = function(words, k) {
+    let obj = {}, res = [];
+    
+    words.forEach(word => {
+        obj[word] = obj[word] ? obj[word] + 1 : 1;
+    });
+    
+    obj = Object.entries(obj).sort((a, b) => a[1] === b[1] ? a[0].localeCompare(b[0]) : b[1] - a[1]);
+    
+    for(let i = 0; i < k; i++)
+    {
+        res.push(obj[i][0])
+    }
+    return res;
+};
