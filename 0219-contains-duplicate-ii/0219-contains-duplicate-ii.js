31e723299d4d7c4560aa376f33ccf1f3ee9fe4e6
Siddharth Patel <siddharthpatel425@gmail.com>
2022-10-21 22:29:01 +0530

    Time: 256 ms (31.01%), Space: 70.5 MB (11.87%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
@@ -4,44 +4,23 @@
  * @return {boolean}
  */
 var containsNearbyDuplicate = function(nums, k) {
-    let left = 0, right = nums.length - 1, obj = {};
+    let left = 0, obj = {};
     
-    while(left <= right)
+    while(left <= nums.length - 1)
     {
-        if(Array.isArray(obj[nums[left]]) && obj[nums[left]].length > 0)
-        {
-           obj[nums[left]].push(left) 
-        }
-        else
+        if(!obj[nums[left]])
         {
             obj[nums[left]] = [left]
         }
+        else
+        {
+            let temp = obj[nums[left]]
+            if(left - temp[temp.length - 1] <= k)
+                return true;
+            else
+                obj[nums[left]].push(left)
+        }
         left++;
     }
-    
-    let arr1 = Object.values(obj)
-    for(let i = 0; i < arr1.length; i++)
-    {
-        let arr = arr1[i];
-        if(arr.length > 1)
-        {
-            let left = 0, success = false;
-            while(left < arr.length - 1)
-            {
-                if(arr[left + 1] - arr[left] <= k)
-                {
-                    success = true;
-                    break;
-                }
-                left++;
-            }
-            
-            if(success)
-            {
-                return true;
-            }
-        }
-    }
-    
     return false;
 };
