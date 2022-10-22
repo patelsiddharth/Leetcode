be830af05171890aa6242fda73b8a20927161777
Siddharth Patel <siddharthpatel425@gmail.com>
2022-10-22 23:14:46 +0530

    Time: 106 ms (24.36%), Space: 41.7 MB (84.62%) - LeetHub

--- 1736-latest-time-by-replacing-hidden-digits/1736-latest-time-by-replacing-hidden-digits.js
+++ 1736-latest-time-by-replacing-hidden-digits/1736-latest-time-by-replacing-hidden-digits.js
@@ -9,11 +9,11 @@ var maximumTime = function(time) {
         if(time[1] === "?")
         {
             time[0] = "2";
-            time[1] = "3"
+            time[1] = "3";
         }
         else
         {
-            time[0] = (parseInt(time[1]) > 3) ? "1" : "2"
+            time[0] = (parseInt(time[1]) > 3) ? "1" : "2";
         }
     }
     else if(time[1] === "?")
@@ -21,14 +21,9 @@ var maximumTime = function(time) {
         time[1] = (time[0] === "2") ? "3" : "9"
     }
     
-    if(time[3] === "?")
-    {
-        time[3] = "5";
-    }
+    (time[3] === "?") && (time[3] = "5");
     
-    if(time[4] === "?")
-    {
-        time[4] = "9";
-    }
+    (time[4] === "?") && (time[4] = "9");
+    
     return time.join("")
 };
