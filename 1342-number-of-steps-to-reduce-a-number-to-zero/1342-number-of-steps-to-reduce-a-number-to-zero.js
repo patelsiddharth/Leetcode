2c2ba0940a53ae867c07f371a200722dc007da59
Siddharth Patel <siddharthpatel425@gmail.com>
2022-10-31 19:07:36 +0530

    Time: 68 ms (88.42%), Space: 42.1 MB (46.39%) - LeetHub

---
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var numberOfSteps = function(num) {
+    let res = 0;
+    
+    while(num !== 0)
+    {
+        if(num % 2 === 0)
+        {
+            num /= 2;
+        }
+        else
+        {
+            num--;
+        }
+        res++;
+    }
+    
+    return res;
+};
