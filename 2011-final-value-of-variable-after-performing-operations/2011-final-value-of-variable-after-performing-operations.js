549db7d8dfcbc976d8d2232a7ef5e67bb5c3e450
Siddharth Patel <siddharthpatel425@gmail.com>
2022-10-31 17:08:28 +0530

    Time: 113 ms (29.71%), Space: 42.4 MB (58.31%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.js
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.js
@@ -3,12 +3,8 @@
  * @return {number}
  */
 var finalValueAfterOperations = function(operations) {
-    let res = 0;
-    operations.forEach(op => {
-        if(op[1] === '+')
-            res++;
-        else
-            res--;
-    })
-    return res;
+    return operations.reduce((res, op) => {
+        res = (op[1] === '+') ? res + 1 : res - 1;
+        return res;
+    }, 0);
 };
