1702cd1e5635e6459db04b4459bff4673956d70b
Siddharth Patel <siddharthpatel425@gmail.com>
2022-11-23 01:36:06 +0530

    Time: 127 ms (22.87%), Space: 45.2 MB (13.30%) - LeetHub

---
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.js
@@ -0,0 +1,61 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var verticalTraversal = function(root) {
+    if(root === null)
+        return null;
+    let queue = [{node : root, x : 0, y: 0}], obj = {}
+    while(queue.length !== 0)
+    {
+        let size = queue.length;
+        for(let i = 0; i < size; i++)
+        {
+            let item = queue.shift()
+            if(!obj[item.x])
+            {
+                obj[item.x] = {}
+                
+                obj[item.x][item.y] = [item.node.val]
+                // obj[item.x] = { item.y : [item.node.val] }
+            }
+            else
+            {
+                if(obj[item.x][item.y])
+                {
+                    obj[item.x][item.y].push(item.node.val)
+                }
+                else
+                {
+                    obj[item.x][item.y] = [item.node.val]
+                }
+            }
+            
+            item.node.left && queue.push({ node : item.node.left, x : item.x - 1, y : item.y + 1})
+            item.node.right && queue.push({ node : item.node.right, x : item.x + 1, y : item.y + 1})
+        }
+    }
+    console.log(obj)
+    let sortedKeys = Object.keys(obj).sort((a,b) => +a - +b)
+    return sortedKeys.reduce((acc, key) => {
+        let temp = []
+        for(y in obj[key]) {
+            temp.push(...obj[key][y].sort((a,b) => a - b))
+        }
+        acc.push(temp)
+        return acc; 
+    }, [])
+    // return []
+    // return Object.keys(obj).sort((a,b) => +a - +b).reduce((acc, curr) => {
+    //     acc.push(obj[curr].sort())
+    //     return acc
+    // }, []);
+};
