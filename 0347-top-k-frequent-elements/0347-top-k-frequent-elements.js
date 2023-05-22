d1541c77d07df0ccb3cd4d7f5416135bd79837c8
Siddharth Patel <siddharthpatel425@gmail.com>
2023-05-23 00:43:12 +0530

    Time: 78 ms (53.79%), Space: 45.8 MB (40.67%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -4,17 +4,26 @@
  * @return {number[]}
  */
 var topKFrequent = function(nums, k) {
-    let obj = {}, res = [];
+    let obj = {}, arr = new Array(nums.length), res = [];
+    
     nums.forEach(num => {
         obj[num] = obj[num] ? obj[num] + 1 : 1;
-    })
-    
-    Object.entries(obj).sort((a,b) => b[1]-a[1]).forEach((val, index) => {
-        if(index < k)
-        {
-            res.push(val[0])
-        }
     });
+    
+    Object.entries(obj).forEach(entry => {
+        if(arr[entry[1]])
+            arr[entry[1]].push(entry[0]);
+        else
+            arr[entry[1]] = [entry[0]];
+    });
+    
+    for(let i = arr.length - 1; i >= 0; i--)
+    {
+        if(res.length < k)
+            arr[i] && res.push(...arr[i]);
+        else
+            break;
+    }
     
     return res;
 };
