4e6beec04a880c8d189a19ac13a7ff43331e8fae
Siddharth Patel <siddharthpatel425@gmail.com>
2023-05-24 00:57:49 +0530

    Time: 1009 ms (26.70%), Space: 48.9 MB (97.51%) - LeetHub

--- 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.js
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.js
@@ -2,11 +2,30 @@
  * @param {number} k
  * @param {number[]} nums
  */
+class List {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
 var KthLargest = function(k, nums) {
-    // this.nums = nums.sort((a,b) => b-a);
-    // this.kthElement = this.nums[k-1];
-    this.nums = nums;
     this.k = k;
+    this.head = null;
+    
+    nums.sort((a,b) => b-a);
+    
+    if(nums.length > 0)
+    {
+        this.head = new List(nums[0]);
+        let curr = this.head;
+        for(let i = 1; i < nums.length; i++)
+        {
+            let node = new List(nums[i]);
+            curr.next = node;
+            curr = curr.next;
+        }
+    }
 };
 
 /** 
@@ -14,9 +33,55 @@ var KthLargest = function(k, nums) {
  * @return {number}
  */
 KthLargest.prototype.add = function(val) {
-    this.nums.push(val)
-    this.nums.sort((a,b) => b-a)
-    return this.nums[this.k-1];
+    let index = 0, res = -1, added = false;
+    let node = new List(val);
+    if(this.head === null)
+    {
+        this.head = new List(val);
+        return val;
+    }
+    else if(this.head.next === null)
+    {
+        if(val > this.head.val)
+        {
+            node.next = this.head
+            this.head = node;
+        }
+        else
+        {
+            this.head.next = node;
+        }
+        return this.k === 1 ? this.head.val : val;
+    }
+    else if(this.head.val <= val)
+    {
+        node.next = this.head;
+        this.head = node;
+        added = true;
+    }
+    
+    let curr = this.head, found = false;
+    
+    while(curr.next != null)
+    {
+        if(index === (this.k-1))
+        {
+            res = curr.val;
+            found = true;
+        }
+        
+        if(!added && curr.val >= val && curr.next.val < val)
+        {
+            node.next = curr.next;
+            curr.next = node;
+            added = true;
+        }
+        
+        curr = curr.next;
+        index++;
+    }
+    
+    return found ? res : curr.val;
 };
 
 /** 
