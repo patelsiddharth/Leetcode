c376c68964cf5170c2eb499b17902a92b23b190e
Siddharth Patel <siddharthpatel425@gmail.com>
2023-11-27 20:16:27 +0530

    Time: 74 ms (50.91%), Space: 44.5 MB (47.89%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,26 +4,27 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    let obj1 = {}, obj2 = {}
+    let obj = {}, obj2 = {}
     s.split("").forEach(i => {
-        obj1[i] = obj1[i] ? obj1[i] + 1 : 1;
-    })
+        obj[i] = obj[i] ? obj[i] + 1 : 1;
+    });
     
-    t.split("").forEach(i => {
-        obj2[i] = obj2[i] ? obj2[i] + 1 : 1;
-    })
-    
-    for(let i in obj1)
+    for(let i of t)
     {
-        if(!obj2[i] || (obj1[i] !== obj2[i]))
+        if(obj[i])
         {
-            return false;
+            obj[i] = obj[i] - 1;
         }
         else
         {
-            delete obj2[i];
+            return false;
+        }
+        
+        if(obj[i] === 0)
+        {
+            delete obj[i];
         }
     }
     
-    return Object.keys(obj2).length === 0;
+    return Object.keys(obj).length === 0;
 };
