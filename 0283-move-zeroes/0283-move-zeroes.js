3b3a16007ef661493988559bf2a3f27a91ee2a12
Siddharth Patel <siddharthpatel425@gmail.com>
2023-12-07 23:27:21 +0530

    Time: 71 ms (80.31%), Space: 46.9 MB (18.76%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -3,18 +3,18 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
+    if(nums.length === 1)
+        return;
+    
     let left = 0;
     for(let idx = 0; idx < nums.length; idx++)
     {
         if(nums[idx] !== 0)
         {
             nums[left] = nums[idx];
+            if(left !== idx)
+                nums[idx] = 0;
             left++;
         }
-    }
-    
-    for(let i = left; i < nums.length; i++)
-    {
-        nums[i] = 0;
     }
 };
