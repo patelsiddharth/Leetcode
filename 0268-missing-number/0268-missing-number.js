984ba7e289b80696cfeea5d9005c12e5f3c4268b
Siddharth Patel <siddharthpatel425@gmail.com>
2023-12-10 23:13:44 +0530

    Time: 67 ms (35.80%), Space: 44.3 MB (56.46%) - LeetHub

--- 0268-missing-number/0268-missing-number.js
+++ 0268-missing-number/0268-missing-number.js
@@ -3,9 +3,12 @@
  * @return {number}
  */
 var missingNumber = function(nums) {
+    let obj = {};
+    nums.forEach(num => obj[num] = 1)
+    
     for(let i = 0; i <= nums.length; i++)
     {
-        if(!nums.includes(i))
+        if(!obj.hasOwnProperty(i))
         {
             return i;
         }
