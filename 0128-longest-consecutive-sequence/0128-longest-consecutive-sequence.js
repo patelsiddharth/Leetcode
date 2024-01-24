66d4dc1e2b544d9fd021e701a818737bde20f236
Siddharth Patel <siddharthpatel425@gmail.com>
2024-01-24 23:01:44 +0530

    Time: 827 ms (35.59%), Space: 82.5 MB (7.64%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
@@ -3,23 +3,24 @@
  * @return {number}
  */
 var longestConsecutive = function(nums) {
-    let obj = {};
+    let obj = {}, max = 0;
+    
     nums.forEach(num => {
         obj[num] = obj[num] ? obj[num] + 1 : 1;
     });
     
-    let maxLen = 0;
-    nums.forEach(num => {
-        if(!obj[num - 1])
+    for(let i = 0; i < nums.length; i++)
+    {
+        if(!obj[nums[i] - 1])
         {
-            let len = 1;
-            while(obj[num + len])
+            let lm = 1;
+            while(obj[nums[i] + lm])
             {
-                len++;
+                lm++;
             }
-            maxLen = Math.max(maxLen, len);
+            max = Math.max(max, lm);    
         }
-    });
+    }
     
-    return maxLen;
+    return max;
 };
