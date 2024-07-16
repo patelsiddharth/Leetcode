24b6c1955aeab9524b1ae627b453f0ad05db1213
Siddharth Patel <siddharthpatel425@gmail.com>
2024-07-16 22:46:03 +0530

    Time: 50 ms (94.93%), Space: 49.6 MB (98.27%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.js
+++ 0229-majority-element-ii/0229-majority-element-ii.js
@@ -6,13 +6,10 @@ var majorityElement = function(nums) {
     let obj = {}, count = Math.floor(nums.length / 3), res = [];
     nums.forEach(num => {
         obj[num] = obj[num] ? obj[num] + 1 : 1;
-    })
-    for(let i in obj)
-    {
-        if(obj[i] > count)
+        if(obj[num] === count + 1)
         {
-            res.push(i);
+            res.push(num);
         }
-    }
+    });
     return res;
 };
