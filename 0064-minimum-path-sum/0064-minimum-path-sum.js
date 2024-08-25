779e4348b56ee44e05a96b769b8e174339c5232a
Siddharth Patel <siddharthpatel425@gmail.com>
2024-08-25 20:51:00 +0530

    Time: 98 ms (5.50%), Space: 64.1 MB (5.39%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.js
+++ 0064-minimum-path-sum/0064-minimum-path-sum.js
@@ -8,12 +8,12 @@ var minPathSum = function(grid) {
         n = grid[0].length;
     
     function move(row, col) {
-        if(row === 0 && col === 0)
+        if(row === m - 1 && col === n - 1)
         {
-            return grid[0][0];
+            return grid[row][col];
         }
         
-        if(row < 0 || col < 0)
+        if(row > m - 1 || col > n - 1)
         {
             return Number.POSITIVE_INFINITY;
         }
@@ -23,28 +23,10 @@ var minPathSum = function(grid) {
             return map.get(row + '+' + col);
         }
         
-        // let down = 0;
-        // if(row + 1 < m)
-        // {
-        //     down = grid[row][col] + move(row + 1, col);
-        // }
-        let up = grid[row][col] + move(row - 1, col);
-        
-        // let right = 0
-        // if(col + 1 < n)
-        // {
-        //     right = grid[row][col] + move(row, col + 1);
-        // }
-        let left = grid[row][col] + move(row, col - 1);
-    
-        let tempMin = Math.min(up, left);
-        
+        let tempMin = grid[row][col] + Math.min(move(row + 1, col), move(row, col + 1));
         map.set(row + '+' + col, tempMin);
-        
         return tempMin;
     }
     
-    let res = move(m-1, n-1);
-    // console.log(map)
-    return res;
+    return move(0, 0);
 };
