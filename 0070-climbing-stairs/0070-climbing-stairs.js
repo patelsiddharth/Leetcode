4121821ce75783765fccf595bc3fb44eb7a3bacb
Siddharth Patel <siddharthpatel425@gmail.com>
2024-08-26 19:46:53 +0530

    Time: 47 ms (76.33%), Space: 48.7 MB (52.64%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.js
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -3,23 +3,25 @@
  * @return {number}
  */
 var climbStairs = function(n) {
-    let obj = {};
-    function climb(x) {
-        if(x === 0 || x === 1)
+    let obj = {}
+    function climb(x)
+    {
+        if(x === 0)
         {
             return 1;
         }
         
-        if(!obj[x-1])
+        if(x < 0)
         {
-            obj[x-1] = climb(x - 1)
-        }
-        if(!obj[x-2])
-        {
-            obj[x-2] = climb(x - 2)
+            return 0;
         }
         
-        return obj[x-1] + obj[x-2];
+        if(obj[x])
+        {
+            return obj[x];
+        }
+        
+        return obj[x] = climb(x-1) + climb(x-2);
     }
     return climb(n);
 };
