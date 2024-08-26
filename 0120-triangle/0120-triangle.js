7fca2d960fd41e57fdbc75cc0ee5f2586cb8eafb
Siddharth Patel <siddharthpatel425@gmail.com>
2024-08-27 00:04:21 +0530

    Time: 51 ms (81.65%), Space: 49.3 MB (76.30%) - LeetHub

--- 0120-triangle/0120-triangle.js
+++ 0120-triangle/0120-triangle.js
@@ -3,15 +3,13 @@
  * @return {number}
  */
 var minimumTotal = function(triangle) {
-    let len = triangle.length, frontRow = triangle[len - 1];
-    
+    let len = triangle.length;
     for(let i = len - 2; i >= 0; i--)
     {
         for(let j = 0; j <= i; j++)
         {
-            frontRow[j] = triangle[i][j] + Math.min(frontRow[j], frontRow[j+1])
+            triangle[len-1][j] = triangle[i][j] + Math.min(triangle[len-1][j], triangle[len-1][j+1])
         }
     }
-    
-    return frontRow[0];
+    return triangle[len-1][0];
 };
