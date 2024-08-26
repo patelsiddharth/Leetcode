3c7a9a65830ea9ab6711eaf0fcbff417670bbb2b
Siddharth Patel <siddharthpatel425@gmail.com>
2024-08-27 00:01:44 +0530

    Time: 58 ms (50.29%), Space: 49.5 MB (64.74%) - LeetHub

--- 0120-triangle/0120-triangle.js
+++ 0120-triangle/0120-triangle.js
@@ -3,28 +3,15 @@
  * @return {number}
  */
 var minimumTotal = function(triangle) {
-    let map = new Map();
-    function move(row, col) {
-        if(row === triangle.length - 1)
-        {
-            return triangle[row][col];
-        }
-        
-        if(map.has(row + '-' + col))
-        {
-            return map.get(row + '-' + col);
-        }
-        
-        let down = triangle[row][col] + move(row + 1, col);
-        
-        let diagonal = triangle[row][col] + move(row + 1, col + 1);
-        
-        let res = Math.min(down, diagonal);
-        
-        map.set(row + '-' + col, res);
-        
-        return res;
-    }
+    let len = triangle.length, frontRow = triangle[len - 1];
     
-    return move(0, 0);
+    for(let i = len - 2; i >= 0; i--)
+    {
+        for(let j = 0; j <= i; j++)
+        {
+            frontRow[j] = triangle[i][j] + Math.min(frontRow[j], frontRow[j+1])
+        }
+    }
+    console.log(frontRow)
+    return frontRow[0];
 };
