c9f70c50faf0efb06f146135dd3b573d4768a35a
Siddharth Patel <siddharthpatel425@gmail.com>
2024-08-31 18:25:53 +0530

    Time: 66 ms (59.02%), Space: 55.6 MB (40.39%) - LeetHub

---
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.js
@@ -0,0 +1,42 @@
+/**
+ * // Definition for a _Node.
+ * function _Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
+ */
+
+/**
+ * @param {_Node} root
+ * @return {_Node}
+ */
+var connect = function(root) {
+    if(root === null)
+    {
+        return root;
+    }
+    
+    let queue = [root];
+    while(queue.length)
+    {
+        let size = queue.length, tempArr = [];
+        for(let i = 0; i < size; i++)
+        {
+            let node = queue.shift();
+            if(queue.length === 0)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = queue[0];
+            }
+            node.left && tempArr.push(node.left);
+            node.right && tempArr.push(node.right);
+        }
+        queue.push(...tempArr);
+    }
+    return root
+};
