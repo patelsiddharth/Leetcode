0ede5d32a03850535a53201b95330c9539309f66
Siddharth Patel <siddharthpatel425@gmail.com>
2024-09-02 00:12:50 +0530

    Time: 361 ms (11.73%), Space: 65.9 MB (13.28%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.js
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.js
@@ -6,24 +6,21 @@ var totalFruit = function(fruits) {
     let l = 0, r = 0, max = 0, obj = {}
     while(r < fruits.length)
     {
-        if(!obj.hasOwnProperty(fruits[r]))
+        obj[fruits[r]] = obj[fruits[r]] ? obj[fruits[r]] + 1 : 1;
+        
+        if(Object.keys(obj).length > 2)
         {
-            obj[fruits[r]] = 1;
-            while(Object.keys(obj).length > 2)
+            obj[fruits[l]]--;
+            if(obj[fruits[l]] === 0)
             {
-                obj[fruits[l]]--;
-                if(obj[fruits[l]] === 0)
-                {
-                    delete obj[fruits[l]];
-                }
-                l++;
+                delete obj[fruits[l]];
             }
+            l++
         }
         else
         {
-            obj[fruits[r]]++;
+            max = Math.max(max, r - l + 1); 
         }
-        max = Math.max(max, r - l + 1);
         r++;
     }
     return max;
