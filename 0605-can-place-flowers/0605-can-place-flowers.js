46bac48a94a172c3f92275c6ec9bae430607d4b8
Siddharth Patel <siddharthpatel425@gmail.com>
2024-09-01 10:32:22 +0530

    Time: 56 ms (69.70%), Space: 51.1 MB (65.59%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.js
+++ 0605-can-place-flowers/0605-can-place-flowers.js
@@ -4,24 +4,33 @@
  * @return {boolean}
  */
 var canPlaceFlowers = function(flowerbed, n) {
+    if(n === 0)
+    {
+        return true;
+    }
+    if(flowerbed.length === 1)
+    {
+        return flowerbed[0] === 0;
+    }
+    
     for(let i = 0; i < flowerbed.length; i++)
     {
         if(
-            flowerbed[i]  === 0 && 
+            flowerbed[i] === 0 &&
             (
-                i === 0 && flowerbed[i + 1] === 0 || 
-                i === flowerbed.length - 1 && flowerbed[i-1] === 0 || 
-                flowerbed[i + 1] === 0 && flowerbed[i-1] === 0 ||
-                flowerbed.length === 1
+                (i === 0 && flowerbed[i + 1] === 0) ||
+                (i === flowerbed.length - 1 && flowerbed[i-1] === 0) ||
+                (flowerbed[i+1] === 0 && flowerbed[i-1] === 0)
             )
         )
         {
             flowerbed[i] = 1;
             n--;
-            if(n <= 0)
-                return true;
+            if(n === 0)
+            {
+                break;
+            }
         }
     }
-    
-    return (n === 0)
+    return n === 0;
 };
