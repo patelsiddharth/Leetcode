eedb88994d181843a300ef64ecd0a87441642434
Siddharth Patel <siddharthpatel425@gmail.com>
2024-09-02 22:14:03 +0530

    Time: 245 ms (5.06%), Space: 56.1 MB (16.88%) - LeetHub

---
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var numberOfSubstrings = function(s) {
+    let l = 0, r = 0, sum   = 0, obj = {};
+    
+    while(r < s.length)
+    {
+        obj[s[r]] = obj.hasOwnProperty(s[r]) ? obj[s[r]] + 1 : 1;
+        
+        while(Object.values(obj).length === 3 && Object.values(obj).every(i => i > 0))
+        {
+            sum += (s.length - r);
+            obj[s[l]]--;
+            l++;
+        }
+        
+        r++;
+    }
+    return sum;
+};   
