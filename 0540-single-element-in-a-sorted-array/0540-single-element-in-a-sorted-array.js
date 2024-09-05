c4f8e2cc8255a8da61ec29ae0fe035bd719faf97
Siddharth Patel <siddharthpatel425@gmail.com>
2024-09-05 19:51:14 +0530

    Time: 44 ms (95.17%), Space: 51.4 MB (88.55%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.js
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.js
@@ -3,26 +3,36 @@
  * @return {number}
  */
 var singleNonDuplicate = function(nums) {
-    let len = nums.length;
-    if(len === 1)
-        return nums[0];
-    
-    let res = 0;
-    for(let i = 0; i < len - 1; i++)
+    if(nums.length === 1)
     {
-        if(nums[i] === nums[i+1])
+        return nums[0];
+    }
+    if(nums[0] !== nums[1])
+    {
+        return nums[0];
+    }
+    if(nums[nums.length - 2] !== nums[nums.length - 1])
+    {
+        return nums[nums.length - 1];
+    }
+    
+    let low = 1, high = nums.length - 2;
+    while(low <= high)
+    {
+        let mid = Math.floor((low+high)/2);
+        if(nums[mid - 1] !== nums[mid] && nums[mid] !== nums[mid + 1])
         {
-            i++;
+            return nums[mid];
+        }
+        
+        if((mid % 2 === 1 && nums[mid - 1] === nums[mid]) || (mid % 2 === 0 && nums[mid] === nums[mid+1]))
+        {
+            low = mid + 1;
         }
         else
         {
-            res = nums[i]
-            break;
+            high = mid - 1;
         }
     }
-    
-    if(nums[len - 1] !== nums[len - 2])
-        return nums[len-1]
-    
-    return res;
+    return -1;
 };
