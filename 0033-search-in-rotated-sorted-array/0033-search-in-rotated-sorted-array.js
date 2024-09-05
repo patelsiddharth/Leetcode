01a4729b5b016db906d95f71248609af5e3e16d4
Siddharth Patel <siddharthpatel425@gmail.com>
2024-09-05 17:58:56 +0530

    Time: 46 ms (87.59%), Space: 48.4 MB (93.72%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -0,0 +1,40 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let low = 0, high = nums.length - 1;
+    while(low <= high)
+    {
+        let mid = Math.floor((low+high)/2);
+        if(nums[mid] === target)
+        {
+            return mid;
+        }
+        
+        if(nums[low] <= nums[mid])
+        {
+            if(nums[mid] > target && nums[low] <= target)
+            {
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+        else if(nums[high] >= nums[mid])
+        {
+            if(nums[mid] < target && nums[high] >= target)
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+    }
+    return -1;
+};
