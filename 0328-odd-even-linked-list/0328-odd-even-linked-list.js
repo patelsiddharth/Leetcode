a9e58ff2119bc3eeb77c62f38da441b5a0f46766
Siddharth Patel <siddharthpatel425@gmail.com>
2024-09-08 01:44:13 +0530

    Time: 74 ms (25.24%), Space: 53.3 MB (38.78%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.js
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.js
@@ -10,25 +10,22 @@
  * @return {ListNode}
  */
 var oddEvenList = function(head) {
-    if(head === null) return null;
-    
-    let temp = head,
-        oddHead = new ListNode(),
-        evenHead = new ListNode(),
-        odd = oddHead,
-        even = evenHead;
-    
-    while(temp !== null)
+    if(head === null || head.next === null || head.next.next === null)
     {
-        odd.next = temp;
-        odd = odd.next;
-        
-        even.next = temp.next;
-        even = even.next;
-        
-        temp = (temp.next !== null) ? temp.next.next : null;
+        return head;
     }
     
-    odd.next = evenHead.next;
-    return oddHead.next;
+    let prev = head, curr = head.next, head2 = head.next;
+    
+    while(curr !== null && curr.next !== null)
+    {
+        prev.next = curr.next;
+        curr.next = curr.next.next;
+        prev = prev.next;
+        curr = curr.next;
+    }
+    
+    prev.next = head2;
+    
+    return head;
 };
