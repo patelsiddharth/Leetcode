03a3bfc836dcda0b5efd6a53ea756321ad15d693
Siddharth Patel <siddharthpatel425@gmail.com>
2024-09-22 12:53:31 +0530

    Time: 54 ms (96.90%), Space: 52 MB (89.74%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} piles
+ * @param {number} h
+ * @return {number}
+ */
+var minEatingSpeed = function(piles, h) {
+    let l = 1, r = Math.max(...piles), ans = 1;
+    while(l <= r)
+    {
+        const mid = Math.floor((l+r)/2);
+        // const totalHrs = piles.reduce((acc, curr) => acc + Math.ceil(curr / mid), 0)
+        let totalHrs = 0;
+        for(let i = 0; i < piles.length; i++)
+        {
+            totalHrs += Math.ceil(piles[i] / mid);
+            if(totalHrs > h)
+            {
+                break;
+            }
+        }
+        if(totalHrs <= h)
+        {
+            ans = mid;
+            r = mid - 1;
+        }
+        else if(totalHrs > h)
+        {
+            l = mid + 1;
+        }
+    }
+    
+    return ans;
+};
