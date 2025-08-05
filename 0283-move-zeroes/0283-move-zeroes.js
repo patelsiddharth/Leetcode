ba99c314cb89d721e3a4bfb52c319d6caa778b21
Siddharth Patel <siddharthpatel425@gmail.com>
2025-08-05 16:39:46 +0530

    Time: 1 ms (77.95%), Space: 60.6 MB (37.69%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -4,12 +4,14 @@
  */
 var moveZeroes = function(nums) {
     let left = 0, len = nums.length;
+    
+    if(len === 1) return;
+
     for(let index = 0; index < len; index++) {
         if(nums[index] !== 0) {
-            nums[left++] = nums[index];
+            nums[left] = nums[index];
+            left !== index && (nums[index] = 0);
+            left++;
         }
-    }
-    while(left < len) {
-        nums[left++] = 0;
     }
 };
