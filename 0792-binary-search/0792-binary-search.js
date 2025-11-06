c852afc027ce3c1f1ad7566619d9257debea904b
Siddharth Patel <siddharthpatel425@gmail.com>
2025-11-06 22:34:09 +0530

    Time: 0 ms (100%), Space: 57 MB (89.94%) - LeetHub

--- 0792-binary-search/0792-binary-search.js
+++ 0792-binary-search/0792-binary-search.js
@@ -4,18 +4,16 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    let low = 0, high = nums.length - 1;
-    while(low <= high) {
+    const recur = (low, high) => {
+        if(low > high) return -1;
         let mid = Math.floor((low+high)/2);
-        if (nums[mid] === target) {
+        if(nums[mid] === target) {
             return mid;
         }
-        else if (nums[mid] > target) {
-            high = mid - 1;
+        else if(nums[mid] > target) {
+            return recur(low, mid - 1);
         }
-        else {
-            low = mid + 1;
-        }
+        return recur(mid + 1, high);
     }
-    return -1;
+    return recur(0, nums.length - 1)
 };
