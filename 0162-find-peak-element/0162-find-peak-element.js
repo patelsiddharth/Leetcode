b60ffb8106fac28b9cc5abc7a00173908dca0b9b
Siddharth Patel <siddharthpatel425@gmail.com>
2025-11-13 17:54:29 +0530

    Time: 0 ms (100%), Space: 54.7 MB (18.97%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.js
+++ 0162-find-peak-element/0162-find-peak-element.js
@@ -3,38 +3,31 @@
  * @return {number}
  */
 var findPeakElement = function(nums) {
-    if(nums.length === 1)
-    {
+    let len = nums.length;
+    if (len === 1) {
         return 0;
     }
-    
-    if(nums[0] > nums[1])
-    {
+
+    if (nums[0] > nums[1]) {
         return 0;
     }
-    
-    if(nums[nums.length - 1] > nums[nums.length - 2])
-    {
-        return nums.length - 1;
+    if (nums[len - 1] > nums [len - 2]) {
+        return len - 1;
     }
-    
-    let low = 1, high = nums.length - 2;
-    while(low <= high)
-    {
+
+    let low = 1, high = len - 2;
+    while(low <= high) {
         let mid = Math.floor((low+high)/2);
-        if(nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1])
-        {
+        if (nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]) {
             return mid;
         }
-        
-        if(nums[mid] > nums[mid - 1])
-        {
+
+        if (nums[mid-1] < nums[mid]) {
             low = mid + 1;
-        }
-        else
-        {
+        } else {
             high = mid - 1;
         }
     }
+
     return -1;
 };
