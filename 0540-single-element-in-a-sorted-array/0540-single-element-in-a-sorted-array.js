21a946df713b81af8e4f31ca2ae21480e561f2de
Siddharth Patel <siddharthpatel425@gmail.com>
2025-11-13 12:54:48 +0530

    Time: 0 ms (100%), Space: 57.7 MB (70.28%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.js
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.js
@@ -3,34 +3,21 @@
  * @return {number}
  */
 var singleNonDuplicate = function(nums) {
-    if(nums.length === 1)
-    {
-        return nums[0];
-    }
-    if(nums[0] !== nums[1])
-    {
-        return nums[0];
-    }
-    if(nums[nums.length - 2] !== nums[nums.length - 1])
-    {
-        return nums[nums.length - 1];
-    }
-    
-    let low = 1, high = nums.length - 2;
-    while(low <= high)
-    {
+    let len = nums.length;
+    if (len === 1) return nums[0];
+    if (nums[0] !== nums[1]) return nums[0];
+    if (nums[len-2] !== nums[len - 1]) return nums[len - 1];
+
+    let low = 0, high = len - 1;
+    while(low <= high) {
         let mid = Math.floor((low+high)/2);
-        if(nums[mid - 1] !== nums[mid] && nums[mid] !== nums[mid + 1])
-        {
+        if(nums[mid] !== nums[mid-1] && nums[mid] !== nums[mid+1]) {
             return nums[mid];
         }
-        
-        if((mid % 2 === 1 && nums[mid - 1] === nums[mid]) || (mid % 2 === 0 && nums[mid] === nums[mid+1]))
-        {
+
+        if ((mid % 2 === 0 && nums[mid] === nums[mid+1]) || (mid % 2 === 1 && nums[mid] === nums[mid-1])){
             low = mid + 1;
-        }
-        else
-        {
+        } else {
             high = mid - 1;
         }
     }
