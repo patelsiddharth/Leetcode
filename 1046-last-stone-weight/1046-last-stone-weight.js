007d6959b8816a05c8ed0162d94dc243b199c40b
Siddharth Patel <siddharthpatel425@gmail.com>
2026-01-07 10:37:16 +0530

    Time: 32 ms (7.37%), Space: 60.4 MB (5.47%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.js
+++ 1046-last-stone-weight/1046-last-stone-weight.js
@@ -3,21 +3,21 @@
  * @return {number}
  */
 var lastStoneWeight = function(stones) {
-    while(stones.length > 1)
-    {
-        stones.sort((a,b) => a-b);
-        let len = stones.length - 1;
-        if(stones[len] === stones[len - 1])
-        {
-            stones.pop();
-        }
-        else
-        {
-            stones[len - 1] = stones[len] - stones[len - 1];
-        }
-        
-        stones.pop();
-    }
     
-    return stones[0] || 0;
+    while(stones.length > 0) {
+        stones.sort((a,b) => a-b);
+        const len = stones.length;
+        console.log(len, stones)
+        if (len === 1) {
+            return stones[0];
+        } else if (len === 0) {
+            return 0;
+        } else if(stones[len - 1] === stones[len - 2]) {
+            stones.length = len - 2;
+        } else {
+            stones[len - 2] = stones[len - 1] - stones[len - 2];
+            stones.length = len - 1;
+        }
+    }
+    return 0;
 };
