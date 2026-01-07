7d7c61cd4977d28c46e629656eea05a4c4702850
Siddharth Patel <siddharthpatel425@gmail.com>
2026-01-07 11:57:46 +0530

    Time: 84 ms (7.37%), Space: 65.2 MB (5.47%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.js
+++ 1046-last-stone-weight/1046-last-stone-weight.js
@@ -3,21 +3,50 @@
  * @return {number}
  */
 var lastStoneWeight = function(stones) {
-    
+    const findMax = () => {
+        if (!stones || stones.length < 2) return [-1, -1];
+
+        let max1 = 0, max2 = 1;
+        if (stones[max2] > stones[max1]) {
+            [max1, max2] = [max2, max1];
+        }
+
+        for(let i = 2; i < stones.length; i++) {
+            if (stones[i] > stones[max1]) {
+                max2 = max1;
+                max1 = i;
+            } else if (stones[i] > stones[max2]) {
+                max2 = i;
+            }
+        }
+        return [max1, max2];
+    }
     while(stones.length > 0) {
-        stones.sort((a,b) => a-b);
-        const len = stones.length;
-        console.log(len, stones)
+        let len = stones.length;
         if (len === 1) {
             return stones[0];
         } else if (len === 0) {
             return 0;
-        } else if(stones[len - 1] === stones[len - 2]) {
-            stones.length = len - 2;
-        } else {
-            stones[len - 2] = stones[len - 1] - stones[len - 2];
-            stones.length = len - 1;
         }
+        
+        let [max1, max2] = findMax();
+        console.log(stones)
+        console.log(max1, stones[max1])
+        console.log(max2, stones[max2])
+        console.log(stones[max1] === stones[max2])
+        if(stones[max1] === stones[max2]) {
+            stones.splice(max1, 1);
+            if(max1 > max2) {
+                stones.splice(max2, 1);
+            } else {
+                stones.splice(max2 - 1, 1);
+            }
+            
+        } else {
+            stones[max1] = stones[max1] - stones[max2];
+            stones.splice(max2, 1);
+        }
+            console.log(stones)
     }
     return 0;
 };
