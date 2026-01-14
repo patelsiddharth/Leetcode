2c62815bf389d6e2d397b3c00c6e4a4b5ef1fa70
Siddharth Patel <siddharthpatel425@gmail.com>
2026-01-14 13:11:27 +0530

    Time: 52 ms (58.25%), Space: 53.2 MB (86.49%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.js
+++ 0509-fibonacci-number/0509-fibonacci-number.js
@@ -4,13 +4,10 @@
  */
 var fib = function(n) {
     const dp = Array.from({length : n+1}).map(() => -1);
-    const cal = (x) => {
-        if (x <= 1) return x;
-
-        if (dp[x] !== -1) return dp[x];
-        
-        dp[x] = cal(x - 1) + cal(x - 2);
-        return dp[x];
+    dp[0] = 0;
+    dp[1] = 1;
+    for(let i = 2; i <= n; i++) {
+        dp[i] = dp[i-1] + dp[i-2];
     }
-    return cal(n);
+    return dp[n];
 };
