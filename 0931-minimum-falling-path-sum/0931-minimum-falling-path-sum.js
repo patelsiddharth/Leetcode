1f8da273eb509accdb94fae49f8bc70d5ed928ee
Siddharth Patel <siddharthpatel425@gmail.com>
2026-01-17 14:03:13 +0530

    Time: 10 ms (26.74%), Space: 58.2 MB (39.53%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.js
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.js
@@ -3,27 +3,21 @@
  * @return {number}
  */
 var minFallingPathSum = function(matrix) {
-    let map = new Map();
-    const path = (row, col) => {
-        if (
-            row >= matrix.length || 
-            col >= matrix[0].length || 
-            row < 0 ||
-            col < 0
-        ) return Number.POSITIVE_INFINITY;
-        if (row === matrix.length - 1) return matrix[row][col];
+    let prev = Array.from({ length: matrix[0].length }).fill(0)
+    for(let col = 0; col < matrix[0].length; col++) {
+        prev[col] = matrix[0][col];
+    }
 
-        const key = `${row}-${col}`;
-        if (!map.has(key)) {
-            const res = matrix[row][col] + Math.min(path(row + 1, col - 1), path(row + 1, col), path(row + 1, col + 1));
-            map.set(key, res);
+    for(let i = 1; i < matrix.length; i++) {
+        let curr = Array.from({ length : matrix[0].length }).fill(Number.POSITIVE_INFINITY);
+        for(let j = 0; j < matrix[0].length; j++) {
+            let up = prev[j];
+            let left = j > 0 ? prev[j-1] : Number.POSITIVE_INFINITY;
+            let right = j < matrix[0].length - 1 ? prev[j+1] : Number.POSITIVE_INFINITY;
+            curr[j] = matrix[i][j] + Math.min(up, left, right);
         }
-        return map.get(key);
+        prev = curr
     }
-
-    let min = Number.POSITIVE_INFINITY;
-    for(let i = 0; i < matrix[0].length; i++) {
-        min = Math.min(min, path(0, i));
-    }
-    return min;
+    
+    return Math.min(...prev); 
 };
