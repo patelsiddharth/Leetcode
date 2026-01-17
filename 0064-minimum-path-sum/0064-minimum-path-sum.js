dfdfaa8ed55e1465748b81e3abc3026d96103c48
Siddharth Patel <siddharthpatel425@gmail.com>
2026-01-17 11:18:30 +0530

    Time: 32 ms (10.39%), Space: 62.1 MB (12.08%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.js
+++ 0064-minimum-path-sum/0064-minimum-path-sum.js
@@ -3,15 +3,20 @@
  * @return {number}
  */
 var minPathSum = function(grid) {
-    const map = new Map(), m = grid.length - 1, n = grid[0].length - 1;
-    const path = (i, j) => {
-        if (i === 0 && j === 0) return grid[0][0];
-        if (i < 0 || j < 0) return Number.POSITIVE_INFINITY;
-        const key = `${i}-${j}`;
-        if (!map.has(key)) {
-            map.set(key, grid[i][j] + Math.min(path(i, j - 1), path(i - 1, j)));
+    const m = grid.length, n = grid[0].length;
+    let dp = Array.from({ length : m }).fill(Array.from({ length : n }));
+    
+    for(let i = 0; i < m; i++) {
+        for(let j = 0; j < n; j++) {
+            if (i === 0 && j === 0) dp[0][0] = grid[0][0];
+            else {
+                let left = j > 0 ? dp[i][j-1] : Number.POSITIVE_INFINITY;
+                let up = i > 0 ? dp[i-1][j] : Number.POSITIVE_INFINITY;
+                dp[i][j] = grid[i][j] + Math.min(left, up);
+            }
         }
-        return map.get(key);
     }
-    return path(m, n)
+    console.log(dp)
+    return dp[m-1][n-1];
+
 };
