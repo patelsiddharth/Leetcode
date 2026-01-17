3edebbabbc3625fca47163d81387b74d4caf1bbc
Siddharth Patel <siddharthpatel425@gmail.com>
2026-01-17 11:29:12 +0530

    Time: 6 ms (53.51%), Space: 55.9 MB (84.03%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.js
+++ 0064-minimum-path-sum/0064-minimum-path-sum.js
@@ -4,19 +4,18 @@
  */
 var minPathSum = function(grid) {
     const m = grid.length, n = grid[0].length;
-    let dp = Array.from({ length : m }).fill(Array.from({ length : n }));
-    
+    let prev = Array.from({ length : n }).fill(0);
     for(let i = 0; i < m; i++) {
+        let curr = Array.from({ length : n }).fill(0);
         for(let j = 0; j < n; j++) {
-            if (i === 0 && j === 0) dp[0][0] = grid[0][0];
+            if (i === 0 && j === 0) curr[0] = grid[0][0];
             else {
-                let left = j > 0 ? dp[i][j-1] : Number.POSITIVE_INFINITY;
-                let up = i > 0 ? dp[i-1][j] : Number.POSITIVE_INFINITY;
-                dp[i][j] = grid[i][j] + Math.min(left, up);
+                let left = j > 0 ? curr[j-1] : Number.POSITIVE_INFINITY;
+                let up = i > 0 ? prev[j] : Number.POSITIVE_INFINITY;
+                curr[j] = grid[i][j] + Math.min(left, up);
             }
         }
+        prev = curr;
     }
-    console.log(dp)
-    return dp[m-1][n-1];
-
+    return prev[n-1];
 };
