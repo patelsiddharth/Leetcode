e1a9ec927be9f83556cdde84ca2e651ad71e69fa
Siddharth Patel <siddharthpatel425@gmail.com>
2026-01-19 20:49:20 +0530

    Time: 120 ms (75.78%), Space: 92 MB (26.96%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.js
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.js
@@ -3,23 +3,22 @@
  * @return {number}
  */
 var longestPalindromeSubseq = function(s) {
-    let s1 = s, s2 = s.split('').reverse().join(''), n = s.length;
+    let n = s.length;
     let dp = Array(n).fill().map(() => Array(n).fill(-1));
     const lcs = (idx1, idx2) => {
-        if (idx1 < 0 || idx2 < 0) {
-            return 0
-        }
+        if (idx1 > idx2) return 0;
+        if (idx1 === idx2) return 1;
 
         if(dp[idx1][idx2] !== -1) return dp[idx1][idx2];
 
         let res = 0;
-        if (s1[idx1] === s2[idx2]) {
-            res = 1 + lcs(idx1 - 1, idx2 - 1);
+        if (s[idx1] === s[idx2]) {
+            res = 2 + lcs(idx1 + 1, idx2 - 1);
         } else {
-            res = Math.max(lcs(idx1 - 1, idx2), lcs(idx1, idx2 - 1));
+            res = Math.max(lcs(idx1 + 1, idx2), lcs(idx1, idx2 - 1));
         }
         dp[idx1][idx2] = res;
         return res;
     }
-    return lcs(n-1, n-1);
+    return lcs(0, n-1);
 };
