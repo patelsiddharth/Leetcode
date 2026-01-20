1beb81809530cddc410063cb4075be487cdacdae
Siddharth Patel <siddharthpatel425@gmail.com>
2026-01-20 22:00:34 +0530

    Time: 6 ms (12.54%), Space: 60.8 MB (88.89%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.js
+++ 0002-add-two-numbers/0002-add-two-numbers.js
@@ -15,14 +15,14 @@ var addTwoNumbers = function(l1, l2) {
     let head = new ListNode();
     let curr = head;
     while(curr1 !== null || curr2 !== null) {
-        const val1 = curr1 ? curr1.val : 0;
-        const val2 = curr2 ? curr2.val : 0;
-        let sum = val1 + val2 + carry;
+        let sum = carry;
+        sum += curr1 ? curr1.val : 0;
+        sum += curr2 ? curr2.val : 0;
+
         carry = Math.floor(sum / 10);
-        
-        let node = new ListNode(sum % 10);
-        curr.next = node;
-        curr = node;
+        curr.next = new ListNode(sum % 10);
+
+        curr = curr.next;
         curr1 && (curr1 = curr1.next);
         curr2 && (curr2 = curr2.next);
     }
