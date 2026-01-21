27c8da1baf853ad111b753561f373a644c35fb57
Siddharth Patel <siddharthpatel425@gmail.com>
2026-01-21 20:46:06 +0530

    Time: 62 ms (74.25%), Space: 66.7 MB (45.43%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.js
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.js
@@ -12,18 +12,40 @@
  * @return {ListNode}
  */
 var getIntersectionNode = function(headA, headB) {
-    let map = new Map(), temp = headA;
-    while(temp !== null) {
-        map.set(temp, 1);
+    let temp1 = headA, temp2 = headB, c1 = 0, c2 = 0, prev = null;
+
+    while(temp1 !== null) {
+        c1++;
+        temp1 = temp1.next;
+    }
+    while(temp2 !== null) {
+        c2++;
+        temp2 = temp2.next;
+    }
+    
+    let diff = Math.abs(c1 - c2)
+    let temp = c1 > c2 ? headA : headB;
+    while(diff) {
+        diff--;
         temp = temp.next;
     }
 
-    temp = headB;
-    while (temp !== null) {
-        if (map.has(temp)) {
-            break;
-        }
-        temp = temp.next;
+    if(c1 > c2) {
+        temp1 = temp;
+        temp2 = headB;
+    } else {
+        temp1 = headA;
+        temp2 = temp;
     }
-    return temp;
+
+    while(temp1 !== null && temp2 !== null) {
+        if(temp1 === temp2) {
+            return temp1;
+        }
+
+        temp1 = temp1.next;
+        temp2 = temp2.next;
+    }
+
+    return null;
 };
