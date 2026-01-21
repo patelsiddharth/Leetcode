69779e741a9b2544f4c4848eb8e0370de57796e7
Siddharth Patel <siddharthpatel425@gmail.com>
2026-01-21 12:09:28 +0530

    Time: 14 ms (21.99%), Space: 60.8 MB (23.39%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.js
+++ 0072-edit-distance/0072-edit-distance.js
@@ -5,24 +5,18 @@
  */
 var minDistance = function(word1, word2) {
     const n = word1.length, m = word2.length;
-    
-    // if (n === 0 && m === 0) return 0;
-    // if (n === 1 && m === 0) return 1;
-    // if (n === 0 && m === 1) return 1;
+    const dp = Array(n + 1).fill().map(() => Array(m + 1).fill(0))
+    for(let i = 0; i <= n; i++) dp[i][0] = i;
+    for(let j = 1; j <= m; j++) dp[0][j] = j;
 
-    const dp = Array(n).fill().map(() => Array(m).fill(-1))
-    const match = (i, j) => {
-        if (j < 0) return i + 1;
-        if (i < 0) return j + 1;
-
-        if (dp[i][j] === -1) {
-            if (word1[i] === word2[j]) {
-                dp[i][j] = 0 + match(i - 1, j - 1);
+    for(let i = 1; i <= n; i++) {
+        for(let j = 1; j <= m; j++) {
+            if (word1[i - 1] === word2[j - 1]) {
+                dp[i][j] = 0 + dp[i - 1][j - 1];
             } else {
-                dp[i][j] = 1 + Math.min(match(i - 1, j), match(i, j - 1), match(i - 1, j - 1));
+                dp[i][j] = 1 + Math.min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]);
             }
         }
-        return dp[i][j];
     }
-    return match(n-1, m-1);
+    return dp[n][m];
 };
