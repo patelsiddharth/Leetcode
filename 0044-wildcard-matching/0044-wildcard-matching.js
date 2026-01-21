450b3b34ea4d0841684bb488c195695e1abd0b73
Siddharth Patel <siddharthpatel425@gmail.com>
2026-01-21 13:27:32 +0530

    Time: 137 ms (13.42%), Space: 92.7 MB (37.23%) - LeetHub

---
+++ 0044-wildcard-matching/0044-wildcard-matching.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} s
+ * @param {string} p
+ * @return {boolean}
+ */
+var isMatch = function(s, p) {
+    const n = s.length, m = p.length;
+    const dp = Array(n).fill().map(() => Array(m).fill(-1))
+    const match = (i, j) => {
+        if (i < 0 && j < 0) return true;
+        if (i >= 0 && j < 0) return false;
+        if (i < 0 && j >= 0) {
+            for(let k = 0; k <= j; k++) {
+                if (p[k] !== '*') return false;
+            }
+            return true;
+        }
+
+        if (dp[i][j] === -1) {
+            if (p[j] !== '*' && p[j] !== '?') {
+                if (s[i] !== p[j]) return false;
+                dp[i][j] = match(i - 1, j - 1);
+            } else if (p[j] === '*') {
+                dp[i][j] = match(i - 1, j) + match(i, j - 1);
+            } else if (p[j] === '?') {
+                dp[i][j] = match(i - 1, j - 1);
+            }
+        }
+        return dp[i][j];
+    }
+    return match(n - 1, m - 1);
+};
