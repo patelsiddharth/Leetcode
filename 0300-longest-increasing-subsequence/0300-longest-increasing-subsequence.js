7ac907a603efe5412d4515f6f496f8b2537618d8
Siddharth Patel <siddharthpatel425@gmail.com>
2026-01-21 18:23:36 +0530

    Time: 478 ms (12.25%), Space: 163 MB (5.13%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.js
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.js
@@ -4,20 +4,16 @@
  */
 var lengthOfLIS = function(nums) {
     const n = nums.length;
-    const dp = Array(n).fill().map(() => Array(n+1).fill(-1));
-    const lis = (idx, lastIdx) => {
-        if (idx === n) return 0;
-
-        if (dp[idx][lastIdx+1] === -1) {
+    const dp = Array(n+1).fill().map(() => Array(n+1).fill(0));
+    for(let i = n-1; i >= 0; i--) {
+        for(let j = i-1; j >= -1; j--) {
             let take = 0;
-            if (lastIdx === -1 || nums[idx] > nums[lastIdx]) {
-                take = 1 + lis(idx + 1, idx);
+            if (j === -1 || nums[i] > nums[j]) {
+                take = 1 + dp[i + 1][i + 1];
             }
-            const notTake = 0 + lis(idx + 1, lastIdx);
-            dp[idx][lastIdx+1] = Math.max(take, notTake);
+            const notTake = 0 + dp[i + 1][j + 1];
+            dp[i][j+1] = Math.max(take, notTake);
         }
-
-        return dp[idx][lastIdx+1];
     }
-    return lis(0, -1);
+    return dp[0][0];
 };
