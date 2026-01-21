016db13b3e35034b833115bb8ac651bc38cd428a
Siddharth Patel <siddharthpatel425@gmail.com>
2026-01-21 14:56:22 +0530

    Time: 244 ms (24.75%), Space: 115 MB (16.06%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.js
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.js
@@ -3,25 +3,25 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    const dp = Array(prices.length).fill().map(() => Array(2).fill().map(() => Array(3).fill(-1)));
+    const dp = Array(prices.length).fill().map(() => Array(4).fill(-1));
 
-    const calProfit = (day, buy, cap) => {
-        if (cap === 0 || day === prices.length) return 0;
+    const calProfit = (day, transaction) => {
+        if (transaction === 4 || day === prices.length) return 0;
         
-        if (dp[day][buy][cap] === -1) {
-            if (buy) {
-                const buyStock = -prices[day] + calProfit(day + 1, 0, cap);
-                const notBuyStock = 0 + calProfit(day + 1, 1, cap);
-                dp[day][buy][cap] = Math.max(buyStock, notBuyStock)
+        if (dp[day][transaction] === -1) {
+            if (transaction % 2 === 0) {
+                const buyStock = -prices[day] + calProfit(day + 1, transaction + 1);
+                const notBuyStock = 0 + calProfit(day + 1, transaction);
+                dp[day][transaction] = Math.max(buyStock, notBuyStock)
             }
             else {
-                const sellStock = prices[day] + calProfit(day + 1, 1, cap - 1);
-                const notSellStock = 0 + calProfit(day + 1, 0, cap);
-                dp[day][buy][cap] = Math.max(sellStock, notSellStock)
+                const sellStock = prices[day] + calProfit(day + 1, transaction + 1);
+                const notSellStock = 0 + calProfit(day + 1, transaction);
+                dp[day][transaction] = Math.max(sellStock, notSellStock)
             }
         }
 
-        return dp[day][buy][cap];
+        return dp[day][transaction];
     }
-    return calProfit(0, 1, 2);
+    return calProfit(0, 0);
 };
