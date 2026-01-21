776a54af3333cdf32b53017d8381fc0d153b6716
Siddharth Patel <siddharthpatel425@gmail.com>
2026-01-21 10:49:38 +0530

    Time: 6 ms (51.86%), Space: 84.4 MB (21.44%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.js
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.js
@@ -10,17 +10,34 @@
  * @return {boolean}
  */
 var isPalindrome = function(head) {
-    if (head === null || head.next === null) return head;
+    if (head === null || head.next === null) return true;
     
-    let stack = [], temp = head;
-    while(temp != null) {
-        stack.push(temp.val);
-        temp = temp.next;
+    let prev = null, slow = head, fast = head;
+    while(fast.next != null && fast.next.next != null) {
+        slow = slow.next;
+        fast = fast.next.next;
     }
-    temp = head;
-    while(temp != null) {
-        if (temp.val !== stack.pop()) return false;
-        temp = temp.next;
+    const reverse = (head) => {
+        let temp = head;
+        while(temp != null) {
+            let next = temp.next;
+            temp.next = prev;
+            prev = temp;
+            temp = next;
+        }
+        return prev;
     }
+    const newHead = reverse(slow.next);
+
+    let curr1 = head, curr2 = newHead;
+    while (curr2 != null) {
+        if (curr1.val !== curr2.val) {
+            reverse(newHead);
+            return false;
+        }
+        curr1 = curr1.next;
+        curr2 = curr2.next;
+    }
+    reverse(newHead);
     return true;
 };
