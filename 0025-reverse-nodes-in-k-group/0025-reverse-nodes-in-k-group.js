ef728f8e4a9d8aa4bae08128991ad7ab73f05b2e
Siddharth Patel <siddharthpatel425@gmail.com>
2026-01-22 22:03:40 +0530

    Time: 0 ms (100%), Space: 59.4 MB (46.91%) - LeetHub

---
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.js
@@ -0,0 +1,57 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+const reverse = (head) => {
+    let temp = head, prev = null;
+    while (temp !== null) {
+        let next = temp.next;
+        temp.next = prev;
+        prev = temp;
+        temp = next;
+    }
+    return prev;
+}
+
+const getKthNode = (head, k) => {
+    let temp = head, c = 0;
+    while(temp != null) {
+        c++;
+        if(c === k) {
+            break;
+        }
+        temp = temp.next;
+    }
+    return temp !== null ? temp : null;
+}
+
+var reverseKGroup = function(head, k) {
+    let temp = head, prevNode = null;
+    while(temp !== null) {
+        let kthNode = getKthNode(temp, k);
+        if (kthNode === null) {
+            break;
+        }
+        let nextNode = kthNode.next
+        kthNode.next = null;
+
+        const newHead = reverse(temp);
+
+        if (temp === head) {
+            head = newHead;
+        }
+        temp.next = nextNode;
+        prevNode && (prevNode.next = kthNode);
+        prevNode = temp;
+        temp = temp.next;
+    }
+    return head;
+};
