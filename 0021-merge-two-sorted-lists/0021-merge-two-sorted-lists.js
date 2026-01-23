aeeb8565b9b9fc23d0b75721150da871f45911e4
Siddharth Patel <siddharthpatel425@gmail.com>
2026-01-23 10:27:24 +0530

    Time: 2 ms (12.43%), Space: 58.1 MB (30.81%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -11,31 +11,34 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(list1, list2) {
-    let arr = [], temp1 = list1, temp2 = list2;
+    let temp1 = list1, temp2 = list2;
+    const newHead = new ListNode(0);
+    let prev = newHead;
+
     while(temp1 !== null || temp2 !== null) {
         const val1 = temp1 !== null ? temp1.val : 101, 
               val2 = temp2 !== null ? temp2.val : 101;
 
         if (val1 === val2) {
-            arr.push(val1);
-            arr.push(val2);
             temp1 = temp1.next;
             temp2 = temp2.next;
+            const newNode1 = new ListNode(val1);
+            const newNode2 = new ListNode(val2);
+            newNode1.next = newNode2;
+            prev.next = newNode1;
+            prev = newNode2;
         } else if(val1 > val2) {
-            arr.push(val2);
+            const newNode2 = new ListNode(val2);
+            prev.next = newNode2;
+            prev = newNode2;
             temp2 = temp2.next;
         } else {
-            arr.push(val1);
+            const newNode1 = new ListNode(val1);
+            prev.next = newNode1;
+            prev = newNode1;
             temp1 = temp1.next;
         }
     }
     
-    const newHead = new ListNode(0);
-    let prev = newHead;
-    for(let i = 0; i < arr.length; i++) {
-        const newNode = new ListNode(arr[i]);
-        prev.next = newNode;
-        prev = newNode;
-    }
     return newHead.next;
 };
