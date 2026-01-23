e3c57a14d6ea2db77cbfe7e9965e39498214709d
Siddharth Patel <siddharthpatel425@gmail.com>
2026-01-23 19:23:27 +0530

    Time: 11 ms (4.74%), Space: 60.5 MB (5.17%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.js
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.js
@@ -3,23 +3,14 @@
  * @return {number}
  */
 var singleNonDuplicate = function(nums) {
-    let len = nums.length;
-    if (len === 1) return nums[0];
-    if (nums[0] !== nums[1]) return nums[0];
-    if (nums[len-2] !== nums[len - 1]) return nums[len - 1];
-
-    let low = 0, high = len - 1;
-    while(low <= high) {
-        let mid = Math.floor((low+high)/2);
-        if(nums[mid] !== nums[mid-1] && nums[mid] !== nums[mid+1]) {
-            return nums[mid];
-        }
-
-        if ((mid % 2 === 0 && nums[mid] === nums[mid+1]) || (mid % 2 === 1 && nums[mid] === nums[mid-1])){
-            low = mid + 1;
-        } else {
-            high = mid - 1;
+    const obj = {};
+    for(let num of nums) {
+        obj[num] = obj[num] ? obj[num] + 1 : 1
+    }
+    for (let i in obj) {
+        if(obj[i] === 1) {
+            return parseInt(i);
         }
     }
-    return -1;
+    return 0;
 };
