701992cdbf4a3a22e0485ddae1da2d3326e5b539
Siddharth Patel <siddharthpatel425@gmail.com>
2026-01-24 11:43:19 +0530

    Time: 2 ms (68.78%), Space: 55.8 MB (39.19%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -5,14 +5,13 @@
  */
 var twoSum = function(nums, target) {
     let map = new Map();
+    
     for (let i = 0; i < nums.length; i++) {
-        map.set(nums[i], i);
-    }
-    for (let i = 0; i < nums.length; i++) {
-        let req = target - nums[i];
+        const req = target - nums[i];
         if (map.has(req) && map.get(req) !== i) {
             return [i, map.get(req)]
         }
+        map.set(nums[i], i);
     }
     return [];
 };
