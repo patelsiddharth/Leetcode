aa23b32ddff0b0aee8962687a4dd446a46011f77
Siddharth Patel <siddharthpatel425@gmail.com>
2026-01-24 22:13:36 +0530

    Time: 3 ms (41.63%), Space: 64.2 MB (47.78%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.js
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -4,15 +4,12 @@
  */
 var maxSubArray = function(nums) {
     let max = Number.NEGATIVE_INFINITY, sum = 0;
-    for(let i = 0; i < nums.length; i++)
-    {
+    for(let i = 0; i < nums.length; i++) {
         sum += nums[i];
+        
         max = Math.max(max, sum);
-        if(sum < 0)
-        {
-            sum = 0;
-        }
+
+        if (sum < 0) sum = 0;
     }
-    
     return max;
 };
