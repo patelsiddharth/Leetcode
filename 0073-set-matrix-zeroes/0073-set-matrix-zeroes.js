72aa0083b22e96053b6a6c18de21cd433f5071f6
Siddharth Patel <siddharthpatel425@gmail.com>
2026-01-25 13:06:04 +0530

    Time: 1 ms (94.9%), Space: 58 MB (94.97%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.js
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.js
@@ -3,34 +3,28 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var setZeroes = function(matrix) {
-    for(let row = 0; row < matrix.length; row++)
-    {
-        for(let col = 0; col < matrix[0].length; col++)
-        {
-            if(matrix[row][col] === 0)
-            {
-                for(let k = 0; k < matrix[row].length; k++)
-                {
-                    if(matrix[row][k] !== 0)
-                        matrix[row][k] = "del"
+    const n = matrix.length, m = matrix[0].length;
+    for(let i = 0; i < n; i++) {
+        for(let j = 0; j < m; j++) {
+            if (matrix[i][j] === 0) {
+                for(let k = 0; k < m; k++) {
+                    if (matrix[i][k] !== 0) { 
+                        matrix[i][k] = Number.POSITIVE_INFINITY;
+                    }
                 }
-                for(let k = 0; k < matrix.length; k++)
-                {
-                    if(matrix[k][col] !== 0)
-                        matrix[k][col] = "del"
+                for(let k = 0; k < n; k++) {
+                    if (matrix[k][j] !== 0) {
+                        matrix[k][j] = Number.POSITIVE_INFINITY;
+                    }
                 }
             }
-        }   
+        }
     }
-    
-    for(let row = 0; row < matrix.length; row++)
-    {
-        for(let col = 0; col < matrix[0].length; col++)
-        {
-            if(matrix[row][col] === "del")
-            {
-                matrix[row][col] = 0;
+    for(let i = 0; i < n; i++) {
+        for(let j = 0; j < m; j++) {
+            if (matrix[i][j] === Number.POSITIVE_INFINITY) {
+                matrix[i][j] = 0;
             }
-        }   
+        }
     }
 };
