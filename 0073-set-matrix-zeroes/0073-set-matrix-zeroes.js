2185e37610bd079ed579b2ddd2d17c9992aa0519
Siddharth Patel <siddharthpatel425@gmail.com>
2026-01-25 14:13:17 +0530

    Time: 2 ms (79.09%), Space: 59.5 MB (47.12%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.js
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.js
@@ -4,25 +4,19 @@
  */
 var setZeroes = function(matrix) {
     const n = matrix.length, m = matrix[0].length;
+    const row = Array(n).fill(0), col = Array(m).fill(0);
+
     for(let i = 0; i < n; i++) {
         for(let j = 0; j < m; j++) {
             if (matrix[i][j] === 0) {
-                for(let k = 0; k < m; k++) {
-                    if (matrix[i][k] !== 0) { 
-                        matrix[i][k] = Number.POSITIVE_INFINITY;
-                    }
-                }
-                for(let k = 0; k < n; k++) {
-                    if (matrix[k][j] !== 0) {
-                        matrix[k][j] = Number.POSITIVE_INFINITY;
-                    }
-                }
+                row[i] = 1;
+                col[j] = 1;
             }
         }
     }
     for(let i = 0; i < n; i++) {
         for(let j = 0; j < m; j++) {
-            if (matrix[i][j] === Number.POSITIVE_INFINITY) {
+            if (row[i] === 1 || col[j] === 1) {
                 matrix[i][j] = 0;
             }
         }
