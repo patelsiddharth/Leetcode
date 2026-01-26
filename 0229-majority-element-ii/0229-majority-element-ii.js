8a552a3b85784d5ef2e28f2e089760a3f8460f13
Siddharth Patel <siddharthpatel425@gmail.com>
2026-01-27 00:00:07 +0530

    Time: 2 ms (94.39%), Space: 55.9 MB (91.46%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.js
+++ 0229-majority-element-ii/0229-majority-element-ii.js
@@ -3,16 +3,36 @@
  * @return {number[]}
  */
 var majorityElement = function(nums) {
-    let obj = {}, set = new Set();
     const target = Math.floor(nums.length / 3);
     
+    let res = [], c1 = 0, c2 = 0, el1 = Number.NEGATIVE_INFINITY, el2 = Number.NEGATIVE_INFINITY;
+
     nums.forEach(num => {
-        obj[num] = obj[num] ? obj[num] + 1 : 1;
-        
-        if (obj[num] > target) {
-            set.add(num);
+        if (c1 === 0 && num !== el2) {
+            c1 = 1;
+            el1 = num;
+        } else if (c2 === 0 && num !== el1) {
+            c2 = 1;
+            el2 = num;
+        } else if (num === el1) {
+            c1++;
+        } else if (num === el2) {
+            c2++;
+        } else {
+            c1--;
+            c2--;
         }
     });
 
-    return [...set];
+    c1 = 0;
+    c2 = 0;
+    nums.forEach(num => {
+        if (num === el1) c1++;
+        if (num === el2) c2++;
+    });
+
+    if (c1 > target) res.push(el1);
+    if (c2 > target) res.push(el2);
+
+    return res;
 };
