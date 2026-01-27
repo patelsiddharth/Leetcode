ce6cac54bb9ae8e5806b156d72b87343219bed8b
Siddharth Patel <siddharthpatel425@gmail.com>
2026-01-27 13:49:22 +0530

    Time: 16 ms (63.38%), Space: 58.7 MB (37.66%) - LeetHub

--- 0018-4sum/0018-4sum.js
+++ 0018-4sum/0018-4sum.js
@@ -4,19 +4,28 @@
  * @return {number[][]}
  */
 var fourSum = function(nums, target) {
-    let n = nums.length, res = [], set = new Set();
+    let n = nums.length, res = [];
+
+    nums.sort((a,b) => a-b);
+
     for(let i = 0; i < n; i++){
+        if (i > 0 && nums[i - 1] === nums[i]) continue;
         for(let j = i + 1; j < n; j++) {
-            let map = new Map()
-            for(let k = j + 1; k < n; k++) {
-                const req = target - (nums[i] + nums[j] + nums[k]);
-                if (map.has(req)) {
-                    const arr = [nums[i], nums[j], nums[k], req].sort((a,b) => a-b);
-                    set.add(arr.join(','))
+            if (j > i + 1 && nums[j - 1] === nums[j]) continue;
+            let k = j + 1, l = n - 1;
+            while(k < l) {
+                const sum = nums[i] + nums[j] + nums[k] + nums[l];
+                if (sum < target) k++;
+                else if (sum > target) l--;
+                else {
+                    res.push([nums[i], nums[j], nums[k], nums[l]])
+                    k++;
+                    l--;
+                    while(k < l && nums[k - 1] === nums[k]) k++;
+                    while(k < l && nums[l + 1] === nums[l]) l--;
                 }
-                map.set(nums[k]);
             }
         }
     }
-    return [...set].map(s => s.split(',').map(Number));
+    return res;
 };
