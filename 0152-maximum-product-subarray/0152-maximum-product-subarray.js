3019e1891e09fa4d5bbe947d5ae0f3b0ef1ed674
Siddharth Patel <siddharthpatel425@gmail.com>
2026-01-28 17:43:59 +0530

    Time: 2 ms (82.36%), Space: 54.8 MB (66.11%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.js
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.js
@@ -3,16 +3,14 @@
  * @return {number}
  */
 var maxProduct = function(nums) {
-    let max = nums[0];
-    for(let i = 0; i < nums.length; i++)
-    {
-        let lmax = 1;
-        for(let j = i ; j < nums.length; j++)
-        {
-            lmax = lmax * nums[j]
-            max = Math.max(max, lmax)
-        }
-        
+    let prefix = 1, suffix = 1, len = nums.length, max = Number.NEGATIVE_INFINITY;
+    for (let i = 0; i < nums.length; i++) {
+        if (prefix === 0) prefix = 1;
+        if (suffix === 0) suffix = 1;
+
+        prefix *= nums[i];
+        suffix *= nums[len - 1 - i];
+        max = Math.max(max, Math.max(prefix, suffix));
     }
     return max;
 };
