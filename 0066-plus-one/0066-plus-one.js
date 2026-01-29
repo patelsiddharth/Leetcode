28ecd1baa1c3645247db1321d8ac3f21f79854c7
Siddharth Patel <siddharthpatel425@gmail.com>
2026-01-29 21:59:16 +0530

    Time: 0 ms (100%), Space: 53 MB (86.62%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+    let carry = 1;
+    for(let i = digits.length - 1; i >= 0; i--) {
+        const sum = digits[i] + carry;
+        digits[i] = sum % 10;
+        carry = Math.floor(sum / 10);
+    }
+
+    if (carry) {
+        digits.unshift(1);
+    }
+    return digits;
+};
