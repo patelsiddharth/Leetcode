21735008948319267c61221d6637f2f12265a3e5
Siddharth Patel <siddharthpatel425@gmail.com>
2026-01-29 22:06:54 +0530

    Time: 4 ms (50.83%), Space: 55.2 MB (80.56%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    let stack = []
+    for(let ch of s) {
+        if (ch === '[' || ch === '(' || ch === '{') {
+            stack.push(ch);
+        } else {
+            const popped = stack.pop();
+            if (
+                (ch === ']' && popped !== '[') ||
+                (ch === ')' && popped !== '(') ||
+                (ch === '}' && popped !== '{')
+            ) {
+                return false;
+            }
+        }
+    }
+    return stack.length === 0;
+};
