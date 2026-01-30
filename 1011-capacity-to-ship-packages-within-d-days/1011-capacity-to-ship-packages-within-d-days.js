e548f1f45b4ad8e6e5a0ad86d00bec8312f99c76
Siddharth Patel <siddharthpatel425@gmail.com>
2026-01-30 09:51:57 +0530

    Time: 11 ms (83.56%), Space: 59.7 MB (46.62%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.js
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.js
@@ -4,40 +4,32 @@
  * @return {number}
  */
 var shipWithinDays = function(weights, days) {
-    let sum = weights.reduce((acc,curr) => acc + curr);
-    let max = Math.max(...weights);
-    
-    function getDays(capacity) {
-        let load = 0, days = 1;
-        for(let i = 0; i < weights.length; i++)
-        {
-            if(load + weights[i] > capacity)
-            {
-                days++;
+    let min = Number.POSITIVE_INFINITY, 
+        left = Math.max(...weights), 
+        right = weights.reduce((acc, curr) => acc + curr);
+
+    const calDays = (cap) => {
+        let load = 0, day = 1;
+        for (let i = 0; i < weights.length; i++) {
+            if (load + weights[i] > cap) {
                 load = weights[i];
-            }
-            else
-            {
+                day++;
+            } else {
                 load += weights[i];
             }
         }
-        return days;
+        return day
     }
-    
-    let low = max, high = sum;
-    while(low <= high)
-    {
-        let mid = Math.floor((high + low) / 2);
-        let daysReq = getDays(mid);
-        if(daysReq <= days)
-        {
-            high = mid - 1;
-        }
-        else
-        {
-            low = mid + 1;
+
+    while(left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        const daysToLoad = calDays(mid);
+        if (daysToLoad <= days) {
+            min = Math.min(min, mid);
+            right = mid - 1;
+        } else {
+            left = mid + 1;
         }
     }
-    return low;
-    
+    return min;
 };
