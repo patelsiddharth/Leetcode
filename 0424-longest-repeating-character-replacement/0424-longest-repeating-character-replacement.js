c97b13a6e23fc9654c3293ee5c5391124710f57e
Siddharth Patel <siddharthpatel425@gmail.com>
2026-01-30 22:19:37 +0530

    Time: 74 ms (18.8%), Space: 63.1 MB (6.38%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
@@ -9,6 +9,9 @@ var characterReplacement = function(s, k) {
         obj[s[r]] = obj[s[r]] ? obj[s[r]] + 1 : 1;
         maxFreq = Math.max(maxFreq, obj[s[r]]);
 
+        // r-l+1 represent the lenght of selected substring
+        // maxFreq represents frequency of most occuring character in substring
+        // (r-l+1) - maxFreq represent the no of character to be switched to make all character of substring same
         while((r-l+1) - maxFreq > k) {
             obj[s[l]]--;
             maxFreq = 0;
