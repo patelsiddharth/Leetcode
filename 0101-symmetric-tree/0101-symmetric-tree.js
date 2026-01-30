d502947d9e0fe1e5361a838e3524ecbe570d2565
Siddharth Patel <siddharthpatel425@gmail.com>
2026-01-30 11:55:03 +0530

    Time: 0 ms (100%), Space: 56.3 MB (78.28%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.js
+++ 0101-symmetric-tree/0101-symmetric-tree.js
@@ -11,35 +11,12 @@
  * @return {boolean}
  */
 var isSymmetric = function(root) {
-    if (root === null) return true;
-
-    let queue = [{x: 0, node: root}], level = 0;
-    while(queue.length > 0) {
-        const size  = queue.length;
+    const isSame = (t1, t2) => {
+        if (!t1 && !t2) return true;
+        if ((!t1 && t2) || (t1 && !t2) || (t1.val !== t2.val)) return false;
         
-        if(size % 2 !== 0 && level !== 0) return false;
-        level++;
-
-        let left = 0, right = queue.length - 1;
-        while(left < right) {
-            if (
-                queue[left].node.val !== queue[right].node.val ||
-                Math.abs(queue[right].x + queue[left].x) !== 0
-            ) {
-                return false;
-            }
-            left++;
-            right--;
-        }
-        for(let i = 0; i < size; i++) {
-            const {x, node} = queue.shift();
-            if (node.left) {
-                queue.push({x: x-1, node: node.left});
-            } 
-            if (node.right) {
-                queue.push({x: x+1, node: node.right});
-            }
-        }
+        return isSame(t1.left, t2.right) && isSame(t1.right, t2.left);
     }
-    return true;
-};
+
+    return isSame(root.left, root.right);
+}; 
