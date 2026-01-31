797036c6e20084be0c80bcdd7455d3ea16475b93
Siddharth Patel <siddharthpatel425@gmail.com>
2026-01-31 18:00:58 +0530

    Time: 37 ms (5.01%), Space: 64.4 MB (5.23%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var trap = function(height) {
+    let preMax = [], suffMax = [], len = height.length;
+
+    preMax[0] = height[0];
+    suffMax[len - 1] = height[len - 1];
+
+    for(let i = 1; i < len; i++) {
+        preMax[i] = Math.max(preMax[i - 1], height[i]);
+    }
+
+    for(let i = len - 2; i >= 0; i--) {
+        suffMax[i] = Math.max(suffMax[i + 1], height[i]);
+    }
+    console.log(preMax, suffMax)
+
+    let total = 0;
+    for(let i = 0; i < len; i++) {
+        if (height[i] < preMax[i] && height[i] < suffMax[i]) {
+            total += Math.min(preMax[i], suffMax[i]) - height[i];
+        }
+    }
+    return total;
+};
