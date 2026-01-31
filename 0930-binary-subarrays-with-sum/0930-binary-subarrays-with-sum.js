d275b75af927a47559e2d8dc91c7844150256520
Siddharth Patel <siddharthpatel425@gmail.com>
2026-01-31 11:49:02 +0530

    Time: 1 ms (96.74%), Space: 56.1 MB (90.49%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.js
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.js
@@ -4,24 +4,21 @@
  * @return {number}
  */
 var numSubarraysWithSum = function(nums, goal) {
-    function subSum(k) {
-        if(k < 0) return 0;
-        let l = 0, r = 0, sum = 0, count = 0;
-        while(r < nums.length)
-        {
+    const subSum = (goal) => {
+        if (goal < 0) return 0;
+
+        let l = 0, r = 0, count = 0, sum = 0;
+        while(r < nums.length) {
             sum += nums[r];
-            
-            while(sum > k)
-            {
-                sum -= nums[l]
-                l++
+            while(sum > goal) {
+                sum -= nums[l];
+                l++;
             }
-            
-            count = count + (r - l + 1)
+            count += r-l+1;
             r++;
         }
+
         return count;
     }
-    
     return subSum(goal) - subSum(goal - 1);
 };
