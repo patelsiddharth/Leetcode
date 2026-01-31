80cc9a968b1189a532907c27f0aa96c0a19baeb9
Siddharth Patel <siddharthpatel425@gmail.com>
2026-01-31 13:20:08 +0530

    Time: 6 ms (25.93%), Space: 57.4 MB (63.67%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.js
+++ 0496-next-greater-element-i/0496-next-greater-element-i.js
@@ -4,27 +4,22 @@
  * @return {number[]}
  */
 var nextGreaterElement = function(nums1, nums2) {
-    let len = nums2.length, res = [], obj = {}; // last = -1;
-    // obj[nums2[len - 1]] = -1;
-    for(let i = 0; i < len; i++) {
-        obj[nums2[i]] = -1;
-        for(let j = i+1; j < len; j++) {
-            if(nums2[j] > nums2[i]) {
-                obj[nums2[i]] = nums2[j];
-                break;
+    let stack = [], obj = {}, res = [];
+    for(let i = nums2.length - 1; i >= 0; i--) {
+        if (stack.length === 0) {
+            obj[nums2[i]] = -1;
+        } else {
+            let len = stack.length;
+            while(stack[len - 1] < nums2[i] && len > 0) {
+                len--;
             }
+            stack.length = len;
+            obj[nums2[i]] = (len === 0) ? -1 : stack[len-1];
         }
+        stack.push(nums2[i]);
     }
-    // for(let i = nums2.length - 2; i >= 0; i--) {
-    //     obj[nums2[i]] = nums2[i + 1] > nums2[i] ? nums2[i+1] : last;
-    //     last = obj[nums2[i]];
-    // }
-    // console.log(obj)
-    for(let item of nums1) {
-        res.push(obj[item]);
+    for(let i of nums1) {
+        res.push(obj[i]);
     }
     return res;
 };
-
-// 1,3,2,4
-// 3,4,4,-1
