c8048d5e7320c18c867fbed289c268803c38b4d3
Siddharth Patel <siddharthpatel425@gmail.com>
2026-01-31 13:23:29 +0530

    Time: 4 ms (50.66%), Space: 58.3 MB (34.4%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.js
+++ 0496-next-greater-element-i/0496-next-greater-element-i.js
@@ -6,16 +6,10 @@
 var nextGreaterElement = function(nums1, nums2) {
     let stack = [], obj = {}, res = [];
     for(let i = nums2.length - 1; i >= 0; i--) {
-        if (stack.length === 0) {
-            obj[nums2[i]] = -1;
-        } else {
-            let len = stack.length;
-            while(stack[len - 1] < nums2[i] && len > 0) {
-                len--;
-            }
-            stack.length = len;
-            obj[nums2[i]] = (len === 0) ? -1 : stack[len-1];
+        while(stack[stack.length - 1] < nums2[i] && stack.length > 0) {
+            stack.pop();
         }
+        obj[nums2[i]] = (stack.length === 0) ? -1 : stack[stack.length - 1];
         stack.push(nums2[i]);
     }
     for(let i of nums1) {
