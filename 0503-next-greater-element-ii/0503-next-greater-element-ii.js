7dd47fb9e03fb46906ef614d6b495267be1df920
Siddharth Patel <siddharthpatel425@gmail.com>
2026-01-31 16:40:21 +0530

    Time: 15 ms (19.1%), Space: 62.9 MB (35.7%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.js
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.js
@@ -5,13 +5,14 @@
 var nextGreaterElements = function(nums) {
     let n = nums.length, stack = [], res = [];
     for(let i = 2*n - 1; i >= 0; i--) {
-        while(stack.length !== 0 && stack[stack.length - 1] <= nums[i%n]) {
+        const idx = i % n;
+        while(stack.length !== 0 && stack[stack.length - 1] <= nums[idx]) {
             stack.pop();
         }
         if (i < n) {
             res[i] = (stack.length === 0) ? -1 : stack[stack.length - 1];
         }
-        stack.push(nums[i%n]);
+        stack.push(nums[idx]);
     }
     return res;
 };
