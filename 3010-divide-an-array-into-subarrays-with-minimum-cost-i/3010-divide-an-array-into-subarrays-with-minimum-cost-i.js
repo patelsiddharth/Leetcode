a370ce90483ed7e3ac158a0e042d1e4ac7bb3d19
Siddharth Patel <siddharthpatel425@gmail.com>
2026-02-01 17:09:28 +0530

    Time: 1 ms (88.46%), Space: 56 MB (73.08%) - LeetHub

---
+++ 3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minimumCost = function(nums) {
+    let min1 = 51, min2 = 51;
+
+    for(let i = 1; i < nums.length; i++) {
+        if (nums[i] < min1) {
+            min2 = min1;
+            min1 = nums[i];
+        } else if (nums[i] < min2) {
+            min2 = nums[i];
+        }
+    }
+
+    return nums[0] + min1 + min2;
+};
