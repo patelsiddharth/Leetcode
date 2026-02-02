d03a6da2b39a70aac56d2704669e15cb659906d1
Siddharth Patel <siddharthpatel425@gmail.com>
2026-02-02 10:04:18 +0530

    Time: 138 ms (5.02%), Space: 87.5 MB (5.02%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.js
+++ 0739-daily-temperatures/0739-daily-temperatures.js
@@ -3,16 +3,14 @@
  * @return {number[]}
  */
 var dailyTemperatures = function(temperatures) {
-    let stack = [], res = [];
-
+    let res = [], stack = []
     for(let i = temperatures.length - 1; i >= 0; i--) {
-        while (stack.length !== 0 && stack[stack.length - 1][0] <= temperatures[i]) {
+        while(stack.length !== 0 && stack[stack.length - 1][0] <= temperatures[i]) {
             stack.pop();
         }
-        res[i] = stack.length !== 0 ? stack[stack.length - 1] : [-1,-1];
+        res[i] = stack.length === 0 ? [-1, -1] : stack[stack.length - 1];
         stack.push([temperatures[i], i]);
     }
-    
     return res.map((item, idx) => {
         return item[1] === -1 ? 0 : item[1] - idx;
     })
