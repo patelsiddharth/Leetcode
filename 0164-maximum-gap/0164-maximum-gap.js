306e37ecc3ccce4d888f1007bd099b96ef8b78c2
Siddharth Patel <siddharthpatel425@gmail.com>
2026-02-03 10:33:48 +0530

    Time: 119 ms (79.17%), Space: 88.6 MB (19.58%) - LeetHub

--- 0164-maximum-gap/0164-maximum-gap.js
+++ 0164-maximum-gap/0164-maximum-gap.js
@@ -3,9 +3,16 @@
  * @return {number}
  */
 var maximumGap = function(nums) {
-    nums.sort((a,b) => a - b), max = 0;
-    for(let i = 1; i < nums.length; i++) {
-        max = Math.max(max, nums[i] - nums[i-1]);
+    let max = 0, obj = {};
+
+    for(let i = 0; i < nums.length; i++) {
+        obj[nums[i]] = nums[i]
     }
+    
+    let arr = Object.values(obj);
+    for(let i = 1; i < arr.length; i++) {
+        max = Math.max(max, arr[i] - arr[i-1]);
+    }
+    
     return max;
 };
