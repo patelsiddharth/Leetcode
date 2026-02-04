37ec558055b23d6ffebe71cedffe48e20c5a0ddb
Siddharth Patel <siddharthpatel425@gmail.com>
2026-02-04 23:31:40 +0530

    Time: 12 ms (10.59%), Space: 62.7 MB (13.45%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.js
+++ 0039-combination-sum/0039-combination-sum.js
@@ -4,23 +4,20 @@
  * @return {number[][]}
  */
 var combinationSum = function(candidates, target) {
-    const map = new Map();
-    const findSum = (i, sum , arr) => {
-        if (sum > target) return;
-
-        if (i === candidates.length) {
-            if (sum === target) {
-                if (!map.has(arr.toString())) {
-                    map.set(arr.toString(), arr);
-                }
-            }
+    let res = []
+    const path = (i, sum, arr) => {
+        if (sum === target) {
+            res.push(arr);
+            return;
+        }
+        
+        if (i >= candidates.length || sum > target) {
             return;
         }
 
-        findSum(i, sum + candidates[i], [...arr, candidates[i]]);
-        findSum(i+1, sum, arr);
-        // findSum(i+1, sum + candidates[i], [...arr, candidates[i]]);
+        path(i, sum + candidates[i], [...arr, candidates[i]]);
+        path(i + 1, sum, arr);
     }
-    findSum(0, 0, []);
-    return Array.from(map.values());
+    path(0, 0, []);
+    return res;
 };
