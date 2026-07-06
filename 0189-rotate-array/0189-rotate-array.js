b627b92018e31fd54c7797145fb23d76e1eb3501
Siddharth Patel <siddharthpatel425@gmail.com>
2026-07-06 20:08:40 +0530

    Time: 1 ms (83.12%), Space: 63.4 MB (85.61%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.js
+++ 0189-rotate-array/0189-rotate-array.js
@@ -4,17 +4,18 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var rotate = function(nums, k) {
-    const len = nums.length;
-    k = k % len;
-    const reverse = (i, j) => {
-        while (i < j) {
-            [nums[i], nums[j]] = [nums[j], nums[i]];
+    const swap = (i, j) => {
+        while(i <= j) {
+            const temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
             i++;
             j--;
         }
     }
-
-    reverse(0, len - 1 - k);
-    reverse(len - k, len - 1);
-    reverse(0, len - 1);
+    let len = nums.length;
+    k = k % len;
+    swap(0, len - 1 - k);
+    swap(len - k, len - 1);
+    swap(0, len - 1);
 };
