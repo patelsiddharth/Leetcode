cfae91d0739cd4d63f4fe0de5ab34cddc16232d3
Siddharth Patel <siddharthpatel425@gmail.com>
2026-07-08 16:44:54 +0530

    Time: 2 ms (68.81%), Space: 59.4 MB (57.5%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.js
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.js
@@ -3,14 +3,14 @@
  * @return {number}
  */
 var findMaxConsecutiveOnes = function(nums) {
-    let max = 0, count = 0;
-    for(let idx = 0; idx < nums.length; idx++) {
-        if (nums[idx] === 1) {
-            count++;
-            max = Math.max(max, count);
+    let maxZeroCount = 0, zeroCount = 0;
+    for (let idx = 0; idx < nums.length; idx++) {
+        if (nums[idx] === 0) {
+            zeroCount = 0;
         } else {
-            count = 0;
+            zeroCount++;
         }
+        maxZeroCount = Math.max(maxZeroCount, zeroCount);
     }
-    return max;
+    return maxZeroCount;
 };
