fc7c6a42d6d69c5122fd8e576bb0a639dde53036
Siddharth Patel <siddharthpatel425@gmail.com>
2026-08-24 20:09:41 +0530

    Time: 0 ms (100%), Space: 57.1 MB (42.66%) - LeetHub

--- 0268-missing-number/0268-missing-number.js
+++ 0268-missing-number/0268-missing-number.js
@@ -4,14 +4,10 @@
  */
 var missingNumber = function(nums) {
     let n = nums.length, xor1, xor2;
-    const arr = new Array(n+1);
-    nums.forEach(num => {
-        xor2 ^= num
-    })
-    for(let i = 0; i < n+1; i++) {
-        arr[i] = i;
-        xor1 ^= i;
+    for(let i = 0; i < n; i++) {
+        xor1 ^= nums[i];
+        xor2 ^= i;
     }
-
+    xor2 ^= n;
     return xor1 ^ xor2;
 };
